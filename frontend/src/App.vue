ba595d50cc42f494156d482aaf63e735f02f2d9d
Nathan Smithies <nts36@uclive.ac.nz>
2021-03-04 10:52:31 +1300

    S302T17-8 #time 70m Redesigned profile page and added fields ?implement

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div id="app">
     <!-- Sizes your content based upon application components -->
-    <RegistrationPage/>
+    <RegistrationPage 
+    fullName="Tim Tam" 
+    nickname="Timmy" 
+    bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    email="tim.tam@hotmail.com"
+    dateOfBirth="1/1/1900"
+    phoneNo="0800 83 83 83"
+    address="10 Downing Street"
+    />
   </div>
 </template>
 
--- frontend/src/components/RegistrationPage.vue
+++ frontend/src/components/RegistrationPage.vue
@@ -1,7 +1,62 @@
 <template>
-  <div>
-    <img class="profile-img" src="https://edit.co.uk/uploads/2016/12/Image-1-Alternatives-to-stock-photography-Thinkstock.jpg"/>
-  </div>
+  <v-main>
+    <v-card class="body">
+      <div>
+        <div class="profile-img">
+          <v-avatar size="200px">
+            <img src="https://edit.co.uk/uploads/2016/12/Image-1-Alternatives-to-stock-photography-Thinkstock.jpg"/>
+          </v-avatar>
+        </div>
+        <div class="names">
+          <h1>
+            {{ fullName }}
+          </h1>
+          <h2><i>
+            {{ nickname }}
+          </i></h2>
+        </div>
+      </div>
+
+      <div class="fields">
+        <v-simple-table>
+          <tbody>
+            <tr>
+              <td>
+                Email
+              </td>
+              <td>
+                {{ email }}
+              </td>
+              <td>
+                Date of Birth
+              </td>
+              <td>
+                {{ dateOfBirth }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Phone Number
+              </td>
+              <td>
+                {{ phoneNo }}
+              </td>
+              <td>
+                Home Address
+              </td>
+              <td>
+                {{ address }}
+              </td>
+            </tr>
+            </tbody>
+        </v-simple-table>
+      </div>
+
+      <div>
+        {{ bio }}
+      </div>
+    </v-card>
+  </v-main>
 </template>
 
 <script lang="ts">
@@ -18,6 +73,10 @@ const RegistrationPage = {
 
   mounted() {
   },
+
+  props: ['fullName', 'nickname', 'bio', 'email', 'dateOfBirth', 'phoneNo', 'address'], 
+  // props: ['id'], 
+  // TODO fetch properties from the store instead
 
   // computed properties
   // http://vuejs.org/guide/computed.html
@@ -44,6 +103,32 @@ export default RegistrationPage;
 
 <style scoped>
 .profile-img {
-  border-radius: 50%;
+  float: left;
+  margin-top: -116px;
+  margin-right: 16px;
 }
+
+.body {
+  padding: 16px;
+  width: 100%;
+}
+
+.names {
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.fields {
+  clear: both;
+}
+
+td:nth-child(1),td:nth-child(3) {
+  font-weight: bold;
+  text-align: right;
+}
+
+tbody tr:hover {
+  background-color: transparent !important;
+}
+
 </style>
