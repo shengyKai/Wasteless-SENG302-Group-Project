dae4f9948f285740e0e91701088c7cf1318619b8
Ben <ben.s5757@gmail.com>
2021-03-04 23:18:19 +1300

    Auth/Profile toggle
    Can toggle between Auth and Profile pages by changing the value of 'loggedIn'.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,19 +2,17 @@
   <div id="app">
     <v-app>
       <v-main>
-        <!--<ProfilePage 
-        fullName="Tim Tam" 
-        nickname="Timmy" 
-        bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        email="tim.tam@hotmail.com"
-        dateOfBirth="1/1/1900"
-        phoneNo="0800 83 83 83"
-        address="10 Downing Street"
-        />-->
-
         <!-- IF: Logged In -->
         <div v-if="loggedIn">
-          <Students />
+          <ProfilePage 
+            fullName="Tim Tam" 
+            nickname="Timmy" 
+            bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            email="tim.tam@hotmail.com"
+            dateOfBirth="1/1/1900"
+            phoneNo="0800 83 83 83"
+            address="10 Downing Street"
+          />
         </div>
 
         <!-- IF: Logged Out -->
