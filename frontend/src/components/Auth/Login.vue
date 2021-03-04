4d8bbdc7c29acff69bb7b75c7f979ca5017f36a6
Phua Sheng-He <shp55@uclive.ac.nz>
2021-03-04 14:25:31 +1300

    Removed v-app tag from login page ?implement

--- frontend/src/components/Auth/Login.vue
+++ frontend/src/components/Auth/Login.vue
@@ -1,25 +1,23 @@
 <template>
-  <v-app>
-    <v-container>
-      <v-form v-model="valid" class="login">
-        <h1>Sign in</h1>
-        <v-text-field
-            v-model="email"
-            type="email"
-            label="Email"
-            :rules="usernameRules"
-        ></v-text-field>
-        <v-text-field
-            v-model="password"
-            type="password"
-            label="Password"
-            :rules="passwordRules"
-            ></v-text-field>
-        <v-btn @click="showProfile" color="primary" :disabled="!valid">Log in</v-btn>
-        <v-btn id="register-button" @click="showRegister" color="primary">Register</v-btn>
-      </v-form>
-    </v-container>
-  </v-app>
+  <v-container>
+    <v-form v-model="valid" class="login">
+      <h1>Sign in</h1>
+      <v-text-field
+          v-model="email"
+          type="email"
+          label="Email"
+          :rules="usernameRules"
+      ></v-text-field>
+      <v-text-field
+          v-model="password"
+          type="password"
+          label="Password"
+          :rules="passwordRules"
+      ></v-text-field>
+      <v-btn @click="showProfile" color="primary" :disabled="!valid">Log in</v-btn>
+      <v-btn id="register-button" @click="showRegister" color="primary">Register</v-btn>
+    </v-form>
+  </v-container>
 </template>
 
 <script>
@@ -31,6 +29,7 @@ export default {
       email: '',
       password: '',
       usernameRules: [
+
         email => !!email || 'Email is required',
         email => !!email || /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email) || 'E-mail must be valid'
       ],
