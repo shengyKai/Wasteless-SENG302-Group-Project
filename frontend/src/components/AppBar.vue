f59a784766d1d25acd1e58c3f2b96c51c51bef84
Ben <ben.s5757@gmail.com>
2021-03-06 10:38:41 +1300

    S302T17-9 #time 1h Created app bar ?implement
    Created app bar, tidied the main stylesheet, and changed the component order in App.vue.

---
+++ frontend/src/components/AppBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-app-bar max-height="64px">
+    <div class="container-outer">
+      <div class="container-inner">
+        <h1>WASTELESS</h1>
+      </div>
+    </div>
+  </v-app-bar>
+</template>
