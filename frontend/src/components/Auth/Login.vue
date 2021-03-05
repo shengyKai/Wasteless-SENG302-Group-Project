929aa3e5b9834cdd0032889cf5856a090cdd825e
Edward Wong <ezw21@uclive.ac.nz>
2021-03-05 13:48:39 +1300

    testing button with icon and adding condition check in buetify button, working on redirecting from login page to profile without going back to app.vue

--- frontend/src/components/Auth/Login.vue
+++ frontend/src/components/Auth/Login.vue
@@ -14,14 +14,50 @@
           label="Password"
           :rules="passwordRules"
       ></v-text-field>
-      <v-btn id="login-button" @click="showProfile" color="primary" :disabled="!valid">Log in</v-btn>
+      <v-btn id="login-button" @click="showProfile" color="primary">Log in</v-btn>    <!--- :disabled="!valid" -->
+      <!-- Add in if-else check in ^^^ button-->
+      <!-- redirect to profile page -->
+              <!-- IF: Logged In -->
+        <div v-if="loggedIn">
+          <ProfilePage 
+            fullName="Tim Tam" 
+            nickname="Timmy" 
+            bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            email="tim.tam@hotmail.com"
+            dateOfBirth="1/1/1900"
+            phoneNo="0800 83 83 83"
+            address="10 Downing Street"
+          />
+        </div>
+
+        <!-- IF: Logged Out -->
+        <div v-else>
+          <Auth />
+        </div>
+        <v-btn
+        class="ma-2"
+        color="purple"
+        dark
+      >
+        <v-icon dark>
+          mdi-minus-circle
+        </v-icon>
+      </v-btn>  
+
+      <v-btn depressed class="pink white--text" @click="showLogin">
+        <v-icon lefet>email</v-icon>
+        <span>nav</span>
+      </v-btn>
       <v-btn id="register-button" @click="showRegister" color="primary">Register</v-btn>
     </v-form>
   </v-container>
 </template>
 
 <script>
+
 export default {
+
+  loggedIn: true,
   name: 'Login',
   data() {
     return {
@@ -38,16 +74,20 @@ export default {
       ]
     }
   },
+
+  
   methods: {
     showRegister() {
       this.$emit('showRegister');
     },
-    showProfile() {
+    showProfile() { //hereeeeeeeeee
       alert('TODO');
+      this.$emit('showProfile');
     }
   }
 }
 </script>
+
 
 <style scoped>
 .button-row {
