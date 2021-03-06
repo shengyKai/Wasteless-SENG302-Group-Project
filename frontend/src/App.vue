47ca60f1a99fbee891db77e2409a8ba32344920e
Ben <ben.s5757@gmail.com>
2021-03-06 23:11:21 +1300

    Fixed App.vue
    Fixed router issues in App.vue.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,7 +7,7 @@
         <div class="container-outer">
           <div class="container-inner">
             <!-- All content (except AppBar & Footer) should be a child of 'v-main'. -->
-              <ProfilePage />
+            <router-view />
           </div>
         </div>
       </v-main>
@@ -29,7 +29,6 @@
 </template>
 
 <script>
-import ProfilePage from "./components/ProfilePage.vue";
 import Auth from "./components/Auth";
 import AppBar from "./components/AppBar";
 import Footer from './components/Footer';
@@ -51,7 +50,6 @@ export default {
   components: {
     // list your components here to register them (located under 'components' folder)
     // https://vuejs.org/v2/guide/components-registration.html
-    ProfilePage,
     Auth,
     AppBar,
     Footer
