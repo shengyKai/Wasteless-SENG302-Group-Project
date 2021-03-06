6587e90b2149829a371b536125def2cd78c08a22
Nathan Smithies <nts36@uclive.ac.nz>
2021-03-06 21:38:15 +1300

    S302T17-18 #time 10m Added link support to search results

--- frontend/src/components/SearchResultItem.vue
+++ frontend/src/components/SearchResultItem.vue
@@ -5,10 +5,9 @@
         </v-list-item-avatar>
         <v-list-item-content>
             <v-list-item-title>
-                <!--TODO Wait for vue-router to be set up correctly -->
-                <!--<router-link :to="{ name: 'profile', params: { 'id': user.id }}"> -->
+                <router-link :to="{ name: 'profile', params: { 'id': user.id }}" class="link">
                     {{ user.firstName }} {{ user.lastName }} 
-                <!-- </router-link> -->
+                </router-link>
             </v-list-item-title>
             <v-list-item-subtitle> {{ user.email }} </v-list-item-subtitle>
             <v-list-item-subtitle> {{ user.homeAddress }} </v-list-item-subtitle>
@@ -21,3 +20,15 @@ export default {
     props: ['user'],
 }
 </script>
+
+<style scoped>
+.link {
+    color: black;
+    text-decoration: none;
+}
+
+.link:hover {
+    text-decoration: underline;
+}
+
+</style>
