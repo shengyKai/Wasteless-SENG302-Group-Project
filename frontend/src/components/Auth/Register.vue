3303efd2236a0bb69cee0c59d90e9a9eed9bc555
Phua Sheng-He <shp55@uclive.ac.nz>
2021-03-12 03:28:22 +1300

    S302T17-15 #time 3h quickfix to datepicker field, limiting user to not choose dates beyond current date. Still in attempt to fix address fields. ?fix

--- frontend/src/components/Auth/Register.vue
+++ frontend/src/components/Auth/Register.vue
@@ -5,46 +5,46 @@
     <v-container>
       <!-- INPUT: Email -->
       <v-text-field
-        class="required"
-        v-model="email"
-        label="Email"
-        :rules="mandatoryRules.concat(emailRules)"
-        outlined
+          class="required"
+          v-model="email"
+          label="Email"
+          :rules="mandatoryRules.concat(emailRules)"
+          outlined
       />
 
       <!-- INPUT: Password -->
       <v-text-field
-        class="required"
-        v-model="password"
-        label="Password"
-        @keyup="passwordChange"
-        :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-        :type="showPassword ? 'text' : 'password'"
-        @click:append="showPassword = !showPassword"
-        :rules="mandatoryRules"
-        outlined
+          class="required"
+          v-model="password"
+          label="Password"
+          @keyup="passwordChange"
+          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          :type="showPassword ? 'text' : 'password'"
+          @click:append="showPassword = !showPassword"
+          :rules="mandatoryRules"
+          outlined
       />
 
       <!-- INPUT: Confirm Password -->
       <v-text-field
-        ref="confirmPassword"
-        class="required"
-        v-model="confirmPassword"
-        label="Confirm Password"
-        :append-icon="showConfirmPassword ? 'mdi-eye' : 'mdi-eye-off'"
-        :type="showConfirmPassword ? 'text' : 'password'"
-        @click:append="showConfirmPassword = !showConfirmPassword"
-        :rules="mandatoryRules.concat(passwordConfirmationRule)"
-        outlined
+          ref="confirmPassword"
+          class="required"
+          v-model="confirmPassword"
+          label="Confirm Password"
+          :append-icon="showConfirmPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          :type="showConfirmPassword ? 'text' : 'password'"
+          @click:append="showConfirmPassword = !showConfirmPassword"
+          :rules="mandatoryRules.concat(passwordConfirmationRule)"
+          outlined
       />
 
       <!-- INPUT: Name -->
       <v-text-field
-        class="required"
-        v-model="name"
-        label="Name"
-        :rules="mandatoryRules"
-        outlined
+          class="required"
+          v-model="name"
+          label="Name"
+          :rules="mandatoryRules"
+          outlined
       />
 
       <!-- INPUT: Nickname -->
@@ -56,49 +56,50 @@
 
       <!-- INPUT: Bio -->
       <v-textarea
-        v-model="bio"
-        label="Bio"
-        rows="3"
-        outlined
+          v-model="bio"
+          label="Bio"
+          rows="3"
+          outlined
       />
 
       <!-- INPUT: Date of Birth -->
       <v-dialog
-        ref="dialog"
-        v-model="modal"
-        :return-value.sync="dob"
-        width="300px"
-        persistent
+          ref="dialog"
+          v-model="modal"
+          :return-value.sync="dob"
+          width="300px"
+          persistent
       >
         <template v-slot:activator="{ on, attrs }">
           <v-text-field
-            class="required"
-            v-model="dob"
-            label="Date of Birth"
-            :rules="mandatoryRules"
-            prepend-inner-icon="mdi-calendar"
-            readonly
-            v-bind="attrs"
-            v-on="on"
-            outlined
+              class="required"
+              v-model="dob"
+              label="Date of Birth"
+              :rules="mandatoryRules"
+              prepend-inner-icon="mdi-calendar"
+              readonly
+              v-bind="attrs"
+              v-on="on"
+              outlined
           />
         </template>
         <v-date-picker
-          v-model="dob"
-          scrollable
+            v-model="dob"
+            :max="currentDate"
+            scrollable
         >
           <v-spacer></v-spacer>
           <v-btn
-            text
-            color="primary"
-            @click="closeDatePicker"
+              text
+              color="primary"
+              @click="closeDatePicker"
           >
             Cancel
           </v-btn>
           <v-btn
-            text
-            color="primary"
-            @click="$refs.dialog.save(dob)"
+              text
+              color="primary"
+              @click="$refs.dialog.save(dob)"
           >
             OK
           </v-btn>
@@ -107,32 +108,107 @@
 
       <!-- INPUT: Phone -->
       <v-text-field
-        v-model="phone"
-        label="Phone"
-        outlined
+          v-model="phone"
+          label="Phone"
+          outlined
       />
 
       <!-- INPUT: Address -->
       <v-combobox
-        class="required"
-        v-model="address"
-        label="Address"
-        :rules="mandatoryRules"
-        :items="items"
-        :loading="isLoading"
-        :search-input.sync="search"
-        item-text="name"
-        item-value="symbol"
-        no-filter
-        clearable
-        outlined
+          class="required"
+          v-model="address"
+          label="Address"
+          :rules="mandatoryRules"
+          :items="items"
+          :loading="isLoading"
+          :search-input.sync="search"
+          no-filter
+          clearable
+          outlined
       />
 
+      <v-combobox
+          class="required"
+          v-model="country"
+          label="Country"
+          :rules="mandatoryRules"
+          :items="countryItems"
+          :loading="isLoading"
+          :search-input.sync="search"
+          no-filter
+          clearable
+          outlined
+      />
+
+      <v-combobox
+          class="required"
+          v-model="state"
+          label="State"
+          :rules="mandatoryRules"
+          :items="items"
+          :loading="isLoading"
+          :search-input.sync="search"
+          no-filter
+          clearable
+          outlined
+      />
+
+      <v-combobox
+          class="required"
+          v-model="city"
+          label="City"
+          :rules="mandatoryRules"
+          :items="items"
+          :loading="isLoading"
+          :search-input.sync="search"
+          no-filter
+          clearable
+          outlined
+      />
+
+      <v-combobox
+          class="required"
+          v-model="houseOrBlockNumber"
+          label="House/Block Number"
+          :rules="mandatoryRules"
+          :items="items"
+          :loading="isLoading"
+          :search-input.sync="search"
+          no-filter
+          clearable
+          outlined
+      />
+
+      <v-combobox
+          class="required"
+          v-model="street"
+          label="Street"
+          :rules="mandatoryRules"
+          :items="items"
+          :loading="isLoading"
+          :search-input.sync="search"
+          no-filter
+          clearable
+          outlined
+      />
+
+      <v-combobox
+          class="required"
+          v-model="postcode"
+          label="Postcode"
+          :rules="mandatoryRules"
+          :items="items"
+          :loading="isLoading"
+          :search-input.sync="search"
+          no-filter
+          clearable
+          outlined
+      />
 
       <!-- Login button if user already has an account. -->
       <p
-        class="link"
-        @click="showLogin"
+          class="link"
+          @click="showLogin"
       >
         Already have an account? Login.
       </p>
@@ -153,7 +229,7 @@
 <script>
 export default {
   name: 'Register',
-  data() {
+  data () {
     return {
       showPassword: false,
       showConfirmPassword: false,
@@ -171,6 +247,15 @@ export default {
       items: [],
       isLoading: false,
       search: null,
+      country: '',
+      countryItems: [],
+      state: '',
+      city: '',
+      houseOrBlockNumber: '',
+      street: '',
+      postcode: '',
+      currentFocus: '',
+      currentDate: new Date().toISOString().slice(0, 10),
       emailRules: [
         //regex rules for emails, example format is as such:
         //"blah@hotmail.co
@@ -180,7 +265,7 @@ export default {
       mandatoryRules: [
         //All fields with the class "required" will go through this ruleset to ensure the field is not empty.
         //if it does not follow the format, display error message
-        field =>  !!field || 'Field is required'
+        field => !!field || 'Field is required'
       ],
 
     }
@@ -188,71 +273,41 @@ export default {
 
   watch: {
     search (val) {
-      let address = '';
-      this.items = [];
-      let addressList = [];
-
       if (val && val.length > 2) {
         this.isLoading = true
-        let url = "https://photon.komoot.io/api/?q=" + val;
+        let url = 'https://photon.komoot.io/api/?q=' + val
 
-        fetch(url)
-          .then(res => res.json())
-          .then(res => {
-            //Address order/format to be presented on Address textfield
-            //Street, City area/District, City/Town/Village, County, Postal code, Country
-
-            //reset addressList for each value changed in the textbox
-            addressList = [];
-
-            //for each address received from the api, extract out the properties of that address
+        fetch(url).then(res => res.json()).then(res => {
+          if (this.currentFocus === 'country') {
+            this.countryItems = []
             res.features.forEach(feature => {
-              if (feature.properties.name !== undefined) {
-                address += feature.properties.name + ", ";
-              }
-              if (feature.properties.district !== undefined) {
-                address += feature.properties.district + ", ";
-              }
-              if (feature.properties.city !== undefined) {
-                address += feature.properties.city + ", ";
-              }
-              if (feature.properties.county !== undefined) {
-                address += feature.properties.county + ", ";
-              }
-              if (feature.properties.postcode !== undefined) {
-                address += feature.properties.postcode + ", ";
-              }
               if (feature.properties.country !== undefined) {
-                address += feature.properties.country + ", ";
+                this.countryItems.push(feature.properties.country)
               }
-              addressList.push(address.substring(0, address.length-2))
-              //reset address
-              address = '';
             })
-            //set the items in the combobox
-            this.items = addressList;
-          })
-          .catch(err => {
-            console.log(err)
-          })
-          .finally(() => (this.isLoading = false))
+          }
+        }).catch(err => {
+          console.log(err)
+        }).finally(() => (this.isLoading = false))
       }
     },
+    country: function() {
+      this.currentFocus = 'country'
+    }
   },
 
-  
   methods: {
     // Show login screen
-    showLogin() {
-      this.$emit('showLogin');
+    showLogin () {
+      this.$emit('showLogin')
     },
     // Complete registration with API
-    register() {
-      alert("TODO");
+    register () {
+      alert('TODO')
     },
     // Close the date picker modal
-    closeDatePicker() {
-      this.modal = false;
+    closeDatePicker () {
+      this.modal = false
     },
     //Feature bug:
     //After the user has successfully typed in the same values in the password and confirmPassword, if the user decides
@@ -263,8 +318,8 @@ export default {
     //The bottom method solves that issue by observing the password field. That means the @keyup attribute in the
     //password field observes every finished keystroke in there and calls the ref with "confirmPassword" (in this case
     //it refers to the confirmPassword field)to revalidate itself upon any changes in the password field.
-    passwordChange() {
-      this.$refs.confirmPassword.validate();
+    passwordChange () {
+      this.$refs.confirmPassword.validate()
     },
     querySelections (v) {
       this.loading = true
@@ -283,9 +338,9 @@ export default {
     //change(in this case, each keystroke), and compare it with the password field. If they are not the same,
     //the error message "Passwords must match" will show up at the bottom of the confirmPassword field, until it
     //is the same.
-    passwordConfirmationRule() {
+    passwordConfirmationRule () {
       return () =>
-        this.password === this.confirmPassword || "Passwords must match";
+        this.password === this.confirmPassword || 'Passwords must match'
     }
   }
 }
