91f9631d59b6fcc8235d77084eb7912d0fe40c7a
Nathan Smithies <nts36@uclive.ac.nz>
2021-03-26 14:04:23 +1300

    S302T500-47 #time 20m Fixed UserProfile test and added some documentation ?fix ?document

--- frontend/tests/unit/profile-page.spec.ts
+++ frontend/tests/unit/profile-page.spec.ts
@@ -1,49 +1,43 @@
-import { shallowMount, createLocalVue, Wrapper } from '@vue/test-utils';
+import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Vuetify from 'vuetify';
-import Vuex, { Store, StoreOptions } from 'vuex';
-
-import { createOptions, StoreData } from '@/store';
-import ProfilePage from '@/components/ProfilePage.vue';
+import Vuex from 'vuex';
 import { CombinedVueInstance } from 'vue/types/vue';
+import { shallowMount, createLocalVue, Wrapper } from '@vue/test-utils';
+
+import { createOptions } from '@/store';
+import UserProfile from '@/components/UserProfile.vue';
+
+Vue.use(Vuetify);
 
 const localVue = createLocalVue();
-
 
 localVue.use(VueRouter);
 const router = new VueRouter();
 
-localVue.use(Vuetify);
 
-const vuetify = new Vuetify({
-  theme: {
-    themes: {
-      light: {
-        primary: '#558b2f'
-      }
-    }
-  }
-});
+describe('UserProfile.vue', () => {
+  // Container for the UserProfile under test
+  let wrapper: Wrapper<CombinedVueInstance<UserProfile, object, object, object, Record<never, any>>>;
 
-
-describe('ProfilePage.vue', () => {
-  let wrapper: Wrapper<CombinedVueInstance<ProfilePage, object, object, object, Record<never, any>>>;
-
+  /**
+   * Sets up the test UserProfile instance and populates it with test data.
+   */
   beforeEach(() => {
     localVue.use(Vuex);
     let options = createOptions();
     options.state = {
       user: {
         id: 1,
-        firstName: "firstname",
-        lastName: "lastname",
-        middleName: "middlename",
-        nickname: "nickname",
-        bio: "biography",
-        email: "email_address",
+        firstName: "test_first_name",
+        lastName: "test_last_name",
+        middleName: "test_middle_name",
+        nickname: "test_nickname",
+        bio: "test_biography",
+        email: "test_email_address",
         dateOfBirth: "1/1/1900",
-        phoneNumber: "phone_number",
-        homeAddress: "home_address",
+        phoneNumber: "test_phone_number",
+        homeAddress: "test_home_address",
         created: "1/1/1950",
         role: "user",
         businessesAdministered: [],
@@ -51,7 +45,9 @@ describe('ProfilePage.vue', () => {
     };
     let store = new Vuex.Store(options);
 
-    wrapper = shallowMount(ProfilePage, {
+    const vuetify = new Vuetify();
+
+    wrapper = shallowMount(UserProfile, {
       localVue,
       router,
       vuetify,
@@ -59,26 +55,52 @@ describe('ProfilePage.vue', () => {
     });
   })
 
+  /**
+   * Tests that the UserProfile has the user's first name somewhere in the page
+   */
   it('Renders firstname', () => {
-    expect(wrapper.text()).toContain('firstname')
-  });
-  it('Renders lastname', () => {
-    expect(wrapper.text()).toContain('lastname')
-  });
-  it('Renders nickname', () => {
-    expect(wrapper.text()).toContain('nickname')
-  });
-  it('Renders bio', () => {
-    expect(wrapper.text()).toContain('biography')
-  });
-  it('Renders email', () => {
-    expect(wrapper.text()).toContain('email_address')
-  });
-  it('Renders phone number', () => {
-    expect(wrapper.text()).toContain('phone_number')
-  });
-  it('Renders home address', () => {
-    expect(wrapper.text()).toContain('home_address')
+    expect(wrapper.text()).toContain('test_first_name')
   });
 
+  /**
+   * Tests that the UserProfile has the user's last name somewhere in the page
+   */
+  it('Renders lastname', () => {
+    expect(wrapper.text()).toContain('test_last_name')
+  });
+
+  /**
+   * Tests that the UserProfile has the user's nickname somewhere in the page
+   */
+  it('Renders nickname', () => {
+    expect(wrapper.text()).toContain('test_nickname')
+  });
+
+  /**
+   * Tests that the UserProfile has the user's biography somewhere in the page
+   */
+  it('Renders bio', () => {
+    expect(wrapper.text()).toContain('test_biography')
+  });
+
+  /**
+   * Tests that the UserProfile has the user's email somewhere in the page
+   */
+  it('Renders email', () => {
+    expect(wrapper.text()).toContain('test_email_address')
+  });
+
+  /**
+   * Tests that the UserProfile has the user's phone number somewhere in the page
+   */
+  it('Renders phone number', () => {
+    expect(wrapper.text()).toContain('test_phone_number')
+  });
+
+  /**
+   * Tests that the UserProfile has the user's home address somewhere in the page
+   */
+  it('Renders home address', () => {
+    expect(wrapper.text()).toContain('test_home_address')
+  });
 })
