2044b7e10078d2e0366e902eab9f0fcc8b4448ba
Nathan Smithies <nts36@uclive.ac.nz>
2021-03-29 13:10:56 +1300

    S302T500-19 #time 40m Moved user avatar into separate component ?implement

---
+++ frontend/src/components/utils/UserAvatar.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-avatar
+    :size="sizeInPixels"
+    color="primary"
+    class="white--text headline"
+    :style="`font-size: ${fontSize}px !important`"
+  >
+    {{ user.firstName[0].toUpperCase() }}{{ user.lastName[0].toUpperCase() }}
+    <template v-if="false">
+      <!--In future when we have image support for users we will put it in here-->
+      <img class="profile-image" src="https://edit.co.uk/uploads/2016/12/Image-1-Alternatives-to-stock-photography-Thinkstock.jpg">
+    </template>
+  </v-avatar>
+</template>
+
+<script>
+
+export default{
+  name: 'UserAvatar',
+  props: {
+    user: {},
+    size: {
+      validator: (s) => ['small', 'medium', 'large'].includes(s),
+      default: 'medium',
+    },
+  },
+
+  computed: {
+    sizeInPixels() {
+      switch (this.size) {
+      case "small":
+        return 40;
+      case "medium":
+        return 100;
+      case "large":
+        return 200;
+      default:
+        return 0.0;
+      }
+    },
+    fontSize() {
+      return this.sizeInPixels * 0.5;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.profile-image {
+  margin: auto;
+  position: absolute;
+  top: 0; left: 0; bottom: 0; right: 0;
+}
+</style>
