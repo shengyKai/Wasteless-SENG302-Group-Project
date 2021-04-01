3fc09c033391a9f8f9b72c444eb62c064f5cfe0d
Phua Sheng-He <shp55@uclive.ac.nz>
2021-04-01 16:02:12 +1300

    S302T500-51 #time 10m Changed log in test to a ts file. reduced redundant code. ?test ?fix

--- frontend/tests/unit/login-page.spec.ts
+++ frontend/tests/unit/login-page.spec.ts
@@ -1,37 +1,39 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
-import { createLocalVue, mount } from '@vue/test-utils';
-
-Vue.use(Vuetify);
+import {createLocalVue, mount, Wrapper} from '@vue/test-utils';
 
 import Index from '@/components/Auth/index.vue';
 import Login from '@/components/Auth/Login.vue';
 import Register from '@/components/Auth/Register.vue';
 
+Vue.use(Vuetify);
+
 //This test is to test the page toggling from login to register, but because index.vue has the parent component for
 //the Login component, and is in charged of the toggling, it is used as the mounting point
 describe('index.vue', () => {
+  let wrapper: Wrapper<any>;
   const localVue = createLocalVue();
-  let vuetify;
-  const methodInvoked = jest.spyOn(Login.methods, 'login');
+  let vuetify: Vuetify;
+  const methodInvoked = jest.spyOn((Login as any).methods, 'login');
   beforeEach(() => {
     vuetify = new Vuetify();
-  });
-  it("Testing out the log in page link, should redirect to Register Page from Login Page", async () => {
-    const wrapper = mount(Index, {
+    wrapper = mount(Index, {
       localVue,
       vuetify,
+      methodInvoked,
+      components: {
+        Login,
+        Register
+      },
       data() {
         return {
           //override value for login to true, meaning the current page is at Login page
           login: true
         };
-      },
-      components: {
-        Login,
-        Register
       }
-    });
+    } as any);
+  });
+  it("Testing out the log in page link, should redirect to Register Page from Login Page", async () => {
     //if login is true, the Login component should exist and the Register component should not exist
     expect(wrapper.findComponent(Login).exists()).toBeTruthy();
     expect(wrapper.findComponent(Register).exists()).toBeFalsy();
@@ -48,21 +50,6 @@ describe('index.vue', () => {
 
   it("Testing out the log in page button, should call function login which redirects to Profile Page from " +
     "Login Page", async () => {
-    const wrapper = mount(Index, {
-      localVue,
-      vuetify,
-      methodInvoked,
-      components: {
-        Login
-      },
-      data() {
-        return {
-          //override value for login to true, meaning the current page is at Login page
-          login: true
-        };
-      }
-    });
-
     //initially wrapper should not be able to find UserProfile page as its in the Login page
     //checking Login page existence
     expect(wrapper.findComponent(Login).exists()).toBeTruthy();
@@ -92,20 +79,20 @@ describe('index.vue', () => {
 });
 
 describe('Login.vue', () => {
+  let wrapper: Wrapper<any>;
   const localVue = createLocalVue();
-  let vuetify;
+  let vuetify: Vuetify;
 
   beforeEach(() => {
     vuetify = new Vuetify();
+    wrapper = mount(Login, {
+      localVue,
+      vuetify
+    });
   });
 
   it("Testing out the inputs for the email and password, such that the user can only press the login button " +
     "after inputting valid formats for both fields", async () => {
-    const wrapper = mount(Login, {
-      localVue,
-      vuetify
-    });
-
     //find the login button by the component
     const loginButton = wrapper.find(".v-btn");
     //initial value of disabled should be true, since both fields are empty
@@ -132,10 +119,6 @@ describe('Login.vue', () => {
   });
 
   it("Testing for invalid email format, with less than two characters after each '.'", async () => {
-    const wrapper = mount(Login, {
-      localVue,
-      vuetify
-    });
     const loginButton = wrapper.find(".v-btn");
     expect(loginButton.props().disabled).toBeTruthy();
 
@@ -149,10 +132,6 @@ describe('Login.vue', () => {
   });
 
   it("Testing for invalid email format,with no '@'", async () => {
-    const wrapper = mount(Login, {
-      localVue,
-      vuetify
-    });
     const loginButton = wrapper.find(".v-btn");
     expect(loginButton.props().disabled).toBeTruthy();
 
@@ -166,10 +145,6 @@ describe('Login.vue', () => {
   });
 
   it("Testing for invalid email format, with no characters before '@'", async () => {
-    const wrapper = mount(Login, {
-      localVue,
-      vuetify
-    });
     const loginButton = wrapper.find(".v-btn");
     expect(loginButton.props().disabled).toBeTruthy();
 
@@ -183,10 +158,6 @@ describe('Login.vue', () => {
   });
 
   it("Testing for invalid email format, with no '.'", async () => {
-    const wrapper = mount(Login, {
-      localVue,
-      vuetify
-    });
     const loginButton = wrapper.find(".v-btn");
     expect(loginButton.props().disabled).toBeTruthy();
 
@@ -201,10 +172,6 @@ describe('Login.vue', () => {
   });
 
   it("Testing for invalid email format, empty email field", async () => {
-    const wrapper = mount(Login, {
-      localVue,
-      vuetify
-    });
     const loginButton = wrapper.find(".v-btn");
     expect(loginButton.props().disabled).toBeTruthy();
 
@@ -220,10 +187,6 @@ describe('Login.vue', () => {
   // !!!NOTICE!!!
   // this test lags the whole application, so is commented out for the moment
   // it("Testing for invalid email format, over character limit", async () => {
-  //   const wrapper = mount(Login, {
-  //     localVue,
-  //     vuetify
-  //   });
   //   const loginButton = wrapper.find(".v-btn");
   //   expect(loginButton.props().disabled).toBeTruthy();
   //
@@ -239,10 +202,6 @@ describe('Login.vue', () => {
   // });
 
   it("Testing for invalid password format, with no numbers", async () => {
-    const wrapper = mount(Login, {
-      localVue,
-      vuetify
-    });
     const loginButton = wrapper.find(".v-btn");
     expect(loginButton.props().disabled).toBeTruthy();
 
@@ -256,10 +215,6 @@ describe('Login.vue', () => {
   });
 
   it("Testing for invalid password format, with no alphabets", async () => {
-    const wrapper = mount(Login, {
-      localVue,
-      vuetify
-    });
     const loginButton = wrapper.find(".v-btn");
     expect(loginButton.props().disabled).toBeTruthy();
 
@@ -273,10 +228,6 @@ describe('Login.vue', () => {
   });
 
   it("Testing for invalid password format, with less than 7 characters", async () => {
-    const wrapper = mount(Login, {
-      localVue,
-      vuetify
-    });
     const loginButton = wrapper.find(".v-btn");
     expect(loginButton.props().disabled).toBeTruthy();
 
@@ -290,10 +241,6 @@ describe('Login.vue', () => {
   });
 
   it("Testing for invalid password format, empty password field", async () => {
-    const wrapper = mount(Login, {
-      localVue,
-      vuetify
-    });
     const loginButton = wrapper.find(".v-btn");
     expect(loginButton.props().disabled).toBeTruthy();
 
@@ -309,10 +256,6 @@ describe('Login.vue', () => {
   // !!!NOTICE!!!
   // this test lags the whole application, so is commented out for the moment
   // it("Testing for invalid password format, over character limit", async () => {
-  //   const wrapper = mount(Login, {
-  //     localVue,
-  //     vuetify
-  //   });
   //   const loginButton = wrapper.find(".v-btn");
   //   expect(loginButton.props().disabled).toBeTruthy();
   //
