74860a6cc434df1e74bfaaeffba31d47f2ab81be
Nathan Smithies <nts36@uclive.ac.nz>
2021-04-04 19:39:38 +1200

    S302T500-19 #time 40m Fixed layout of user name and thumbnail ?fix
    I've also changed the structure of the appbar into (I think) a
    more sensible state.

--- frontend/src/components/AppBar.vue
+++ frontend/src/components/AppBar.vue
@@ -1,83 +1,76 @@
 <template>
   <v-app-bar max-height="64px">
-    <div class="container-outer">
-      <div class="container-inner">
-        <h1>WASTELESS</h1>
-      </div>
-    </div>
-
     <div v-if="showBusinessDialog">
       <CreateBusiness @closeDialog="showBusinessDialog = false" />
     </div>
 
-    <!-- Search Bar component to perform search and show result -->
-    <SearchBar v-if="$route.path !== '/search'" />
-    <div class="text-center">
-      <v-menu offset-y>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn icon v-bind="attrs" v-on="on">
-            <v-avatar>
-              <v-icon>
-                mdi-account-circle
-              </v-icon>
-            </v-avatar>
-          </v-btn>
-        </template>
-        <v-list class="list">
-          <v-list-item v-if="isDGAA" class="admin link" @click="viewAdmin">
-            <v-list-item-title class="admin">
-              ADMIN
-            </v-list-item-title>
-          </v-list-item>
-          <v-list-item v-else-if="isAdmin" class="admin">
-            <v-list-item-title class="admin">
-              ADMIN
-            </v-list-item-title>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-title class="link" @click="viewProfile">
-              Profile
-            </v-list-item-title>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-title class="link" @click="logout">
-              Logout
-            </v-list-item-title>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-title class="link" @click="viewCreateBusiness">
-              Create Business
-            </v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </div>
+    <div class="container-outer">
+      <h1>WASTELESS</h1>
 
-    <v-chip v-if="user">
-      <UserAvatar :user="user" size="small"/>
-      <div class="name">
-        {{ user.firstName }}
+      <div class="spacer"/>
+
+      <!-- Search Bar component to perform search and show result -->
+      <SearchBar v-if="$route.path !== '/search'" />
+      <div class="text-center">
+        <v-menu offset-y>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon v-bind="attrs" v-on="on">
+              <v-avatar>
+                <v-icon> mdi-account-circle </v-icon>
+              </v-avatar>
+            </v-btn>
+          </template>
+          <v-list class="list">
+            <v-list-item v-if="isDGAA" class="admin link" @click="viewAdmin">
+              <v-list-item-title class="admin"> ADMIN </v-list-item-title>
+            </v-list-item>
+            <v-list-item v-else-if="isAdmin" class="admin">
+              <v-list-item-title class="admin"> ADMIN </v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title class="link" @click="viewProfile">
+                Profile
+              </v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title class="link" @click="logout">
+                Logout
+              </v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title class="link" @click="viewCreateBusiness">
+                Create Business
+              </v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
       </div>
-    </v-chip>
+      <v-chip v-if="user">
+        <UserAvatar :user="user" size="small" />
+        <div class="name">
+          {{ user.firstName }}
+        </div>
+      </v-chip>
+    </div>
   </v-app-bar>
 </template>
 
 <script>
 import SearchBar from "./utils/SearchBar";
-import UserAvatar from './utils/UserAvatar';
+import UserAvatar from "./utils/UserAvatar";
 import CreateBusiness from "./BusinessProfile/CreateBusiness";
-import { USER_ROLES } from '../utils';
+import { USER_ROLES } from "../utils";
 
 export default {
   name: "AppBar",
   components: {
     SearchBar,
     CreateBusiness,
-    UserAvatar
+    UserAvatar,
   },
   data() {
     return {
-      showBusinessDialog: false
+      showBusinessDialog: false,
     };
   },
   methods: {
@@ -100,7 +93,9 @@ export default {
   },
   computed: {
     isAdmin() {
-      return [USER_ROLES.DGAA, USER_ROLES.GAA].includes(this.$store.getters.role);
+      return [USER_ROLES.DGAA, USER_ROLES.GAA].includes(
+        this.$store.getters.role
+      );
     },
     isDGAA() {
       return this.$store.getters.role === USER_ROLES.DGAA;
@@ -108,14 +103,23 @@ export default {
     user() {
       return this.$store.state.user;
     },
-  }
+  },
 };
 </script>
 
 <style scoped>
+
+.spacer {
+  flex: 1;
+  max-width: 900px;
+}
+
 .name {
+  align-self: center;
   margin-left: 5px;
-  color: var(--v-primary-base); /* Not sure why this doesn't set the colour to green */
+  color: var(
+    --v-primary-base
+  ); /* Not sure why this doesn't set the colour to green */
 }
 .admin {
   color: rgb(255, 16, 16);
