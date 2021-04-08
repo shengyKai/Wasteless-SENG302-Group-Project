b1af54d56ab86d76ebdcec771d8177a9d701e5a5
Nathan Smithies <nts36@uclive.ac.nz>
2021-04-08 17:34:44 +1200

    S302T500-37 #time 45m Added inventory to home page and some actions ?implement
    The inventory is only shown if the current role is a business

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -1,7 +1,32 @@
 <template>
   <div class="container">
-    <div class="pane">Left pane</div>
     <div class="newsfeed">
+      <!-- Inventory items -->
+      <v-card
+        v-if="isBusiness"
+        outlined
+        rounded="lg"
+        class="inventory-card"
+      >
+        <v-card-title>
+          Inventory
+        </v-card-title>
+        <v-card-text class="inventory-container">
+          <v-card
+            v-for="(item, index) in inventoryItems"
+            :key="index"
+            class="inventory-item"
+          >
+            <v-img
+              src="https://edit.co.uk/uploads/2016/12/Image-1-Alternatives-to-stock-photography-Thinkstock.jpg"
+              height="50px"
+            />
+            {{ item }}
+          </v-card>
+        </v-card-text>
+      </v-card>
+
+      <!-- Newsfeed -->
       <v-card
         v-for="(item, index) in newsfeedItems"
         :key="index"
@@ -17,7 +42,34 @@
         </v-card-text>
       </v-card>
     </div>
-    <div class="pane">Right pane</div>
+    <v-card
+      rounded="lg"
+      class="action-pane"
+    >
+      <!-- User actions -->
+      <v-card-text>
+        <v-list>
+          <v-list-item-group>
+            <v-list-item @click="viewProfile">
+              <v-list-item-icon>
+                <v-icon>mdi-account-circle</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>Profile</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item @click="viewCreateBusiness">
+              <v-list-item-icon>
+                <v-icon>mdi-briefcase-plus</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>Add Business</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 
@@ -31,37 +83,84 @@ export default {
           content: "Hey this is a comment",
         },
         {
-          title: "10 Shocking facts, number 7 will...",
+          title: "10 Shocking FACTS, Number 7 will...",
           content:
-            "1. The sky is blue\n2. All squares have 4 corners\n3. Exposure to PHP is decreases lifespan by 10 years\n4. I'm out of ideas",
+            "1. The sky is blue\n2. All squares have 4 corners\n3. Exposure to PHP decreases lifespan by 10 years\n4. I'm out of ideas",
         },
       ],
     };
   },
+  methods: {
+    viewProfile() {
+      // TODO It would probably be quite nice to merge this with the appbar implementation, maybe by emitting something
+      switch (this.role.type) {
+      case "user":
+        this.$router.push("/profile");
+        break;
+      case "business":
+        this.$router.push("/business/" + this.$store.state.activeRole.id);
+        break;
+      default:
+        this.$router.push("/profile");
+      }
+    },
 
+    viewCreateBusiness() {
+      // TODO Implement
+      console.warn('Not implemented');
+    }
+  },
   computed: {
+    role() {
+      return this.$store.state.activeRole;
+    },
     isBusiness() {
-      return this.$store.state.activeRole?.type === "business";
+      return this.role?.type === "business";
+    },
+    inventoryItems() {
+      if (!this.isBusiness) return undefined;
+      return [...Array(10).keys()].map(i => `Item ${i}`);
     },
   },
 };
 </script>
 
 <style scoped>
+pre {
+  white-space: pre-wrap;
+}
+
 .container {
   display: flex;
+  max-width: 900px;
+  padding: 10px;
 }
 
 .newsfeed {
-  max-width: 600px;
   flex: 2;
+  min-width: 260px;
+}
+
+.inventory-card {
+  margin-bottom: 10px;
+}
+
+.inventory-container {
+  display: flex;
+  flex-flow: row wrap;
+}
+
+.inventory-item {
+  max-width: 100px;
+  margin: 5px;
 }
 
 .newsfeed-item {
-  margin-top: 10px;
+  margin-bottom: 10px;
 }
 
-.pane {
+.action-pane {
+  margin-left: 10px;
   flex: 1;
 }
 </style>
