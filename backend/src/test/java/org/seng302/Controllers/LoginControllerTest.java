dd30cdc4b848aa704b2bbb68ec5cd261fa5e64b3
Edward Zhan Yip Wong <ezw21@uclive.ac.nz>
2021-04-27 14:52:13 +1200

    S302T500-80 #time 15m fix the test in backend ?fix ?pair[ezw21,egj21]

--- backend/src/test/java/org/seng302/Controllers/LoginControllerTest.java
+++ backend/src/test/java/org/seng302/Controllers/LoginControllerTest.java
@@ -52,7 +52,7 @@ class LoginControllerTest {
                 .withEmail("johnsmith99@gmail.com")
                 .withPassword("1337-H%nt3r2")
                 .withBio("Likes long walks on the beach")
-                .withDob("2021-03-11")
+                .withDob("2001-03-11")
                 .withPhoneNumber("+64 3 555 0129")
                 .withAddress(Location.covertAddressStringToLocation("4,Rountree Street,Christchurch,New Zealand," +
                         "Canterbury,8041"))
