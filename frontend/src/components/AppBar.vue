50ac4354573d3abc891b667a6353856c3f166877
Ben Smith <bts49@uclive.ac.nz>
2021-04-29 02:00:06 +1200

    S302T500-13 #time 60m Mostly finished adding the common theme setup where possible. May be worth creating some custom text fields in the future. Made a few style fixes to AppBar. ?implement

--- frontend/src/components/AppBar.vue
+++ frontend/src/components/AppBar.vue
@@ -6,48 +6,13 @@
       <!-- Search Bar component to perform search and show result, if not on search page -->
       <SearchBar v-if="$route.path !== '/search'" />
 
-      <!-- Right items -->
+      <!-- Profile Menu -->
       <div class="flex-center">
-        <!-- Action menu -->
-        <div class="text-center">
-          <v-menu offset-y>
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn icon v-bind="attrs" v-on="on">
-                <v-avatar>
-                  <v-icon> mdi-account-circle </v-icon>
-                </v-avatar>
-              </v-btn>
-            </template>
-            <v-list class="list">
-              <v-list-item v-if="isDGAA" class="admin link" @click="viewAdmin">
-                <v-list-item-title class="admin"> ADMIN </v-list-item-title>
-              </v-list-item>
-              <v-list-item v-else-if="isAdmin" class="admin">
-                <v-list-item-title class="admin"> ADMIN </v-list-item-title>
-              </v-list-item>
-              <v-list-item>
-                <v-list-item-title class="link" @click="viewProfile">
-                  Profile
-                </v-list-item-title>
-              </v-list-item>
-              <v-list-item>
-                <v-list-item-title class="link" @click="logout">
-                  Logout
-                </v-list-item-title>
-              </v-list-item>
-              <v-list-item>
-                <v-list-item-title class="link" @click="viewCreateBusiness">
-                  Create Business
-                </v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
-        </div>
         <!-- Dropdown menu for selecting role which user is currently acting as -->
         <div class="role-menu">
           <v-menu offset-y>
             <template v-slot:activator="{ on, attrs }">
-              <!-- User name and icon -->
+              <!-- Username and icon -->
               <v-chip
                 v-if="user"
                 v-bind="attrs"
@@ -59,7 +24,13 @@
                 </div>
               </v-chip>
             </template>
+
+            <!-- Menu Items -->
             <v-list>
+              <!-- User/Business Selector -->
+              <div class="section-title">
+                Profiles
+              </div>
               <v-list-item-group
                 v-model="selectedRole"
                 color="primary"
@@ -70,6 +41,37 @@
                   </v-list-item>
                 </template>
               </v-list-item-group>
+
+              <hr>
+
+              <!-- DGAA/GAA Indicator -->
+              <v-list-item v-if="isDGAA" class="admin link" @click="viewAdmin">
+                <v-list-item-title class="admin"> ADMIN </v-list-item-title>
+              </v-list-item>
+              <v-list-item v-else-if="isAdmin" class="admin">
+                <v-list-item-title class="admin"> ADMIN </v-list-item-title>
+              </v-list-item>
+
+              <!-- View User Profile -->
+              <v-list-item>
+                <v-list-item-title class="link" @click="viewProfile">
+                  Profile
+                </v-list-item-title>
+              </v-list-item>
+
+              <!-- Create Business -->
+              <v-list-item>
+                <v-list-item-title class="link" @click="viewCreateBusiness">
+                  Create Business
+                </v-list-item-title>
+              </v-list-item>
+
+              <!-- Logout -->
+              <v-list-item>
+                <v-list-item-title class="link" @click="logout">
+                  Logout
+                </v-list-item-title>
+              </v-list-item>
             </v-list>
           </v-menu>
         </div>
@@ -184,5 +186,10 @@ export default {
   display: flex;
   justify-content: space-between;
   align-items: center;
+}
+
+.section-title {
+  text-align: center;
+  font-weight: 500;
 }
 </style>
