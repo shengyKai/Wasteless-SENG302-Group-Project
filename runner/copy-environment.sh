14465285596384ef504e2b6ec62609b826dffb13
Nathan Smithies <nts36@uclive.ac.nz>
2021-04-29 20:13:14 +1200

    S302T500-73 #time 25m Implemented the application.properties mutation ?implement

--- runner/copy-environment.sh
+++ runner/copy-environment.sh
@@ -1,6 +1,12 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # This script should be executed while in the /backend folder.
 
 echo "Updating application.properties to the current environment"
-echo $DB_URL
+
+
+sed -i -e "s/^spring.jpa.database-platform=.*$/spring.jpa.database-platform=org.hibernate.dialect.MariaDBDialect/" \
+       -e "s/^spring.datasource.url=.*$/spring.datasource.url=$DB_URL/" \
+       -e "s/^spring.datasource.username=.*$/spring.datasource.username=$DB_USERNAME/" \
+       -e "s/^spring.datasource.password=.*$/spring.datasource.password=$DB_PASSWORD/" \
+       ./src/main/resources/application.properties
