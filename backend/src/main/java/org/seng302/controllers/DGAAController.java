6b1b87215fb8fa1564383980607f468fbe4e3c29
Nathan Smithies <nts36@uclive.ac.nz>
2021-04-30 17:52:00 +1200

    Restored DGAA password

--- backend/src/main/java/org/seng302/controllers/DGAAController.java
+++ backend/src/main/java/org/seng302/controllers/DGAAController.java
@@ -54,7 +54,7 @@ public class DGAAController {
                     .withEmail("wasteless@seng302.com")
                     .withFirstName("DGAA")
                     .withLastName("DGAA")
-                    .withPassword("T3amThr33IsTh3")
+                    .withPassword("T3amThr33IsTh3B3st")
                     .withDob("2000-03-11")
                     .withAddress(dgaaAddress)
                     .build();
