fee2096652f8503fe4e2f532e51774d2829a5af6
Nathan Smithies <nts36@uclive.ac.nz>
2021-05-02 16:13:26 +1200

    Fixed typo in copy-environment.sh

--- runner/copy-environment.sh
+++ runner/copy-environment.sh
@@ -9,6 +9,6 @@ sed -i -e "s/^spring.jpa.database-platform=.*$/spring.jpa.database-platform=org.
        -e "s/^spring.datasource.url=.*$/spring.datasource.url=$DB_URL/" \
        -e "s/^spring.datasource.username=.*$/spring.datasource.username=$DB_USERNAME/" \
        -e "s/^spring.datasource.password=.*$/spring.datasource.password=$DB_PASSWORD/" \
-       -e "s/^dgaa.username=.*$/dgaa.username=$DGAA_EMAIL" \
-       -e "s/^dgaa.password=.*$/dgaa.password=$DGAA_PASSWORD" \
+       -e "s/^dgaa.username=.*$/dgaa.username=$DGAA_EMAIL/" \
+       -e "s/^dgaa.password=.*$/dgaa.password=$DGAA_PASSWORD/" \
        ./src/main/resources/application.properties
