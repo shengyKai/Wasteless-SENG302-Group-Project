28f5a70155458540d2c14c23791219497546590f
Nathan Smithies <nts36@uclive.ac.nz>
2021-05-02 16:26:55 +1200

    Added string escaping to copy-environment.sh
    Quite something to overlook, my bad

--- runner/copy-environment.sh
+++ runner/copy-environment.sh
@@ -6,9 +6,9 @@ echo "Updating application.properties to the current environment"
 
 
 sed -i -e "s/^spring.jpa.database-platform=.*$/spring.jpa.database-platform=org.hibernate.dialect.MariaDBDialect/" \
-       -e "s/^spring.datasource.url=.*$/spring.datasource.url=$DB_URL/" \
-       -e "s/^spring.datasource.username=.*$/spring.datasource.username=$DB_USERNAME/" \
-       -e "s/^spring.datasource.password=.*$/spring.datasource.password=$DB_PASSWORD/" \
-       -e "s/^dgaa.username=.*$/dgaa.username=$DGAA_EMAIL/" \
-       -e "s/^dgaa.password=.*$/dgaa.password=$DGAA_PASSWORD/" \
+       -e "s/^spring.datasource.url=.*$/spring.datasource.url=$(echo $DB_URL | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/" \
+       -e "s/^spring.datasource.username=.*$/spring.datasource.username=$(echo $DB_USERNAME | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/" \
+       -e "s/^spring.datasource.password=.*$/spring.datasource.password=$(echo $DB_PASSWORD | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/" \
+       -e "s/^dgaa.username=.*$/dgaa.username=$(echo $DGAA_EMAIL | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/" \
+       -e "s/^dgaa.password=.*$/dgaa.password=$(echo $DGAA_PASSWORD | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/" \
        ./src/main/resources/application.properties
