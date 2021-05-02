a20f3dece7b09b0aa7aaed480b56fc602d6cd414
Nathan Smithies <nts36@uclive.ac.nz>
2021-05-02 16:38:03 +1200

    Added more subsitutions to application.properties

--- runner/copy-environment.sh
+++ runner/copy-environment.sh
@@ -6,6 +6,8 @@ echo "Updating application.properties to the current environment"
 
 
 sed -i -e "s/^spring.jpa.database-platform=.*$/spring.jpa.database-platform=org.hibernate.dialect.MariaDBDialect/" \
+       -e "s/^spring.datasource.driverClassName=.*$/spring.datasource.driverClassName=org.mariadb.jdbc.Driver/" \
+       -e "s/^spring.jpa.properties.hibernate.dialect=.*$/spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.MariaDB103Dialect/" \
        -e "s/^spring.datasource.url=.*$/spring.datasource.url=$(echo $DB_URL | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/" \
        -e "s/^spring.datasource.username=.*$/spring.datasource.username=$(echo $DB_USERNAME | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/" \
        -e "s/^spring.datasource.password=.*$/spring.datasource.password=$(echo $DB_PASSWORD | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/" \
