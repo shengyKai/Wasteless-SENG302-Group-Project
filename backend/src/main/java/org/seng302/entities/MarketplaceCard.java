404fe5882b8a6a10206885626894593ca1b0e2df
Nathan Smithies <nts36@uclive.ac.nz>
2021-05-17 21:56:11 +1200

    S302T500-152 #time 17m Implemented a builder for the MarketplaceCard entity ?implement

--- backend/src/main/java/org/seng302/entities/MarketplaceCard.java
+++ backend/src/main/java/org/seng302/entities/MarketplaceCard.java
@@ -1,7 +1,12 @@
 package org.seng302.entities;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
 import javax.persistence.*;
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 import java.util.Objects;
 
 @Entity
@@ -154,7 +159,129 @@ public class MarketplaceCard {
         return Objects.hash(id, creator, section, title, description, created, closes);
     }
 
+    /**
+     * Valid marketplace card sections
+     */
     public enum Section {
-        FOR_SALE, WANTED, EXCHANGE
+        FOR_SALE("ForSale"),
+        WANTED("Wanted"),
+        EXCHANGE("Exchange");
+
+        private final String name;
+
+        Section(String name) {
+            this.name = name;
+        }
+
+        /**
+         * Gets the name of the section.
+         * Compatible with api
+         * @return section name
+         */
+        public String getName() {
+            return this.name;
+        }
+    }
+
+    /**
+     * This class uses the builder pattern to construct an instance of the MarketplaceCard class
+     */
+    public static class Builder {
+        private User creator;
+        private Section section;
+        private String title;
+        private String description;
+        private Instant closes;
+
+        /**
+         * Sets the builder's creator.
+         *
+         * @param creator Creator for the marketplace card
+         * @return Builder with the creator set
+         */
+        public Builder withCreator(User creator) {
+            this.creator = creator;
+            return this;
+        }
+
+        /**
+         * Sets the builder's section.
+         *
+         * @param section Section for the marketplace card
+         * @return Builder with the section set
+         */
+        public Builder withSection(Section section) {
+            this.section = section;
+            return this;
+        }
+
+        /**
+         * Sets the builder's section.
+         *
+         * @param sectionName Name of the section for the marketplace card
+         * @return Builder with the section set
+         */
+        public Builder withSection(String sectionName) {
+            for (Section possibleSection : Section.values()) {
+                if (possibleSection.getName().equals(sectionName)) {
+                    this.section = possibleSection;
+                    return this;
+                }
+            }
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid section name");
+        }
+
+        /**
+         * Sets the builder's title.
+         *
+         * @param title Title for the marketplace card
+         * @return Builder with the title set
+         */
+        public Builder withTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        /**
+         * Sets the builder's description.
+         *
+         * @param description Description for the marketplace card
+         * @return Builder with the description set
+         */
+        public Builder withDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        /**
+         * Sets the builder's close date and time.
+         * If no closing date is provided then a two week interval after the creation moment is used.
+         *
+         * @param closes Creator for the marketplace card
+         * @return Builder with the closing date and time set
+         */
+        public Builder withCloses(Instant closes) {
+            this.closes = closes;
+            return this;
+        }
+
+        /**
+         * Builds the marketplace card
+         * @return Newly created marketplace card
+         */
+        public MarketplaceCard build() {
+            var card = new MarketplaceCard();
+            card.creator = creator;
+            card.setSection(section);
+            card.setTitle(title);
+            card.setDescription(description);
+            card.created = Instant.now();
+            if (closes == null) {
+                card.setCloses(card.created.plus(2, ChronoUnit.WEEKS));
+            } else {
+                card.setCloses(closes);
+            }
+            return card;
+        }
     }
 }
