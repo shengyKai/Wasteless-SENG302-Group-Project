1d7aaf37b00f73a8db79b39ca329d75bed295d13
Nathan Smithies <nts36@uclive.ac.nz>
2021-05-21 00:15:26 +1200

    S302T500-157 #time 5m Implemented basic keyword entity ?implement

---
+++ backend/src/main/java/org/seng302/entities/Keyword.java
@@ -0,0 +1,71 @@
+package org.seng302.entities;
+
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@NoArgsConstructor
+@Entity
+public class Keyword {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Instant created;
+
+    /**
+     * Constructs a keyword with the given name
+     * @param name name of the new keyword
+     */
+    public Keyword(String name) {
+        created = Instant.now();
+        setName(name);
+    }
+
+    /**
+     * Gets the id (will be unique among keywords)
+     * @return keyword id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Gets the name of this keyword
+     * @return keyword name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Gets the creation date and time for this keyword
+     * @return creation date and time
+     */
+    public Instant getCreated() {
+        return created;
+    }
+
+    /**
+     * Sets and validates the keyword name
+     * @param name new keyword name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Keyword{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", created=" + created +
+                '}';
+    }
+}
