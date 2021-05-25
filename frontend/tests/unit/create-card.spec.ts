481a5c5ebb9acf5c89f6f5356fcba4203cec1f45
Ella Johnson <egj21@uclive.ac.nz>
2021-05-26 09:08:50 +1200

    S302T500-164 #time 12m Started setting up test framework for CreateCard ?test

---
+++ frontend/tests/unit/create-card.spec.ts
@@ -0,0 +1,146 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import Vuetify from 'vuetify';
+import { createLocalVue, Wrapper, mount } from '@vue/test-utils';
+
+import CreateCard from '@/components/marketplace/CreateCard.vue';
+import {castMock} from "./utils";
+import * as api from '@/api/internal';
+import { getStore, resetStoreForTesting } from '@/store';
+import {User} from "@/api/internal";
+
+/**
+ * Creates a test user with the given user id
+ *
+ * @param userId The user id to use
+ * @returns The generated user
+ */
+function makeTestUser(userId: number) {
+  let user: User = {
+    id:  userId,
+    firstName: 'test_firstname' + userId,
+    lastName: 'test_lastname' + userId,
+    nickname: 'test_nickname' + userId,
+    email: 'test_email' + userId,
+    bio: 'test_biography' + userId,
+    phoneNumber: 'test_phone_number' + userId,
+    dateOfBirth: '1/1/1900',
+    created: '1/5/2005',
+    homeAddress: {
+      streetNumber: 'test_street_number',
+      streetName: 'test_street1',
+      city: 'test_city',
+      region: 'test_region',
+      postcode: 'test_postcode',
+      district: 'test_district',
+      country: 'test_country' + userId
+    },
+    businessesAdministered: [],
+  };
+
+
+  return user;
+}
+
+jest.mock('@/api/internal', () => ({
+  createMarketplaceCard: jest.fn(),
+}));
+
+const createMarketplaceCard = castMock(api.createMarketplaceCard);
+Vue.use(Vuetify);
+const localVue = createLocalVue();
+
+/**
+   * Sets up the test CreateCard instance
+   *
+   * Because the element we're testing has a v-dialog we need to take some extra sets to make it
+   * work.
+   */
+describe('CreateCard.vue', () => {
+  // Container for the wrapper around CreateBusiness
+  let appWrapper: Wrapper<any>;
+
+  // Container for the CreateBusiness under test
+  let wrapper: Wrapper<any>;
+
+  beforeEach(() => {
+    const vuetify = new Vuetify();
+    localVue.use(Vuex);
+    resetStoreForTesting();
+    let store = getStore();
+    store.state.user = makeTestUser(1); // log in as user 1
+    // Creating wrapper around CreateBusiness with data-app to appease vuetify
+    const App = localVue.component('App', {
+      components: { CreateCard },
+      template: '<div data-app><CreateBusiness/></div>',
+    });
+
+    // Put the CreateBusiness component inside a div in the global document,
+    // this seems to make vuetify work correctly, but necessitates calling appWrapper.destroy
+    const elem = document.createElement('div');
+    document.body.appendChild(elem);
+
+    // We have to mock the $router.go method to prevent errors.
+    appWrapper = mount(App, {
+      stubs: ['router-link', 'router-view'],
+      mocks: {
+        $router: {
+          go: () => {return;},
+        }
+      },
+      localVue,
+      vuetify,
+      attachTo: elem,
+      store: store,
+    });
+
+    wrapper = appWrapper.getComponent(CreateCard);
+  });
+
+  /**
+   * Executes after every test case.
+   *
+   * This function makes sure that the CreateCard component is removed from the global document
+   */
+  afterEach(() => {
+    appWrapper.destroy();
+  });
+
+  /**
+   * Finds the cancel button in the CreateCard form
+   *
+   * @returns A Wrapper around the cancel button
+   */
+  function findCancelButton() {
+    const buttons = wrapper.findAllComponents({ name: 'v-btn' });
+    const filtered = buttons.filter(button => button.text().includes('Cancel'));
+    expect(filtered.length).toBe(1);
+    return filtered.at(0);
+  }
+
+  /**
+   * Finds the create button in the CreateCard form
+   *
+   * @returns A Wrapper around the create button
+   */
+  function findCreateButton() {
+    const buttons = wrapper.findAllComponents({ name: 'v-btn' });
+    const filtered = buttons.filter(button => button.text().includes('Create Card'));
+    expect(filtered.length).toBe(1);
+    return filtered.at(0);
+  }
+
+  /**
+   * Tests that the CreateCard dialog is valid if all required fields are provided
+   */
+  it('Valid if all required fields are provided', async () => {
+    await wrapper.setData({
+      title: "Title",
+      selectedSection: "ForSale",
+    });
+
+    await Vue.nextTick();
+
+    expect(wrapper.vm.valid).toBeTruthy();
+  });
+});
