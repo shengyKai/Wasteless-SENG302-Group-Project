c7db1fa82660b990b324aa9146fd3089cc2daee0
Ella Johnson <egj21@uclive.ac.nz>
2021-07-05 00:16:03 +1200

    S302T500-261 #time 20m Made ExpiryEvent.vue a bit neater and added expandable view which displays the associated marketplace card ?implement

--- frontend/src/components/home/newsfeed/ExpiryEvent.vue
+++ frontend/src/components/home/newsfeed/ExpiryEvent.vue
@@ -1,28 +1,37 @@
 <template>
   <div>
     <v-card-title>
-      {{ card.title }} is about to expire
+      Your marketplace card is about to expire
     </v-card-title>
     <v-card-subtitle>
       {{ date }}
     </v-card-subtitle>
-    <v-card-actions>
-      <v-btn color="primary">Delay by two weeks</v-btn>
-    </v-card-actions>
     <v-card-text>
-      It will expire in {{ remaining }}
-      <!-- <MarketplaceCard :content="card" style="max-width: 200px"/> -->
+      Your card '{{ card.title }}' will expire in {{ remaining }}. Do you want to delay the expiry by two weeks?
     </v-card-text>
+    <v-expand-transition>
+      <v-container v-show="viewCard">
+        <v-row justify="center">
+          <v-col>
+            <MarketplaceCard :content="card" style="max-width: 300px"/>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-expand-transition>
+    <v-card-actions class="justify-center">
+      <v-btn color="primary">Delay expiry</v-btn>
+      <v-btn color="secondary" @click="viewCard=!viewCard">{{ expandMessage }}</v-btn>
+    </v-card-actions>
   </div>
 </template>
 
 <script>
 import { formatDate } from '@/utils';
-// import MarketplaceCard from '@/components/cards/MarketplaceCard.vue';
+import MarketplaceCard from '@/components/cards/MarketplaceCard';
 
 export default {
-  // components: { MarketplaceCard },
   name: 'ExpiryEvent',
+  components: { MarketplaceCard },
   props: {
     event: {
       type: Object,
@@ -32,24 +41,40 @@ export default {
   data() {
     return {
       now: new Date(),
+      viewCard: false,
     };
   },
   created() {
     setInterval(() => this.now = new Date(), 1000);
   },
   computed: {
+    /**
+     * Access the card associated with this event.
+     */
     card() {
       return this.event.card;
     },
+    /**
+     * The date the event was created in a displayable format.
+     */
     date() {
       return formatDate(this.event.created);
     },
+    /**
+     * The date and time at which the marketplace card expires.
+     */
     expiryDate() {
       return new Date(this.card.displayPeriodEnd);
     },
+    /**
+     * Number of seconds remaining until the marketplace card expires.
+     */
     remainingSeconds() {
       return Math.floor((this.expiryDate - this.now) / 1000);
     },
+    /**
+     * The amount of time remainging before the marketplace card expires in the format <hours>h <minutes>m <seconds>s.
+     */
     remaining() {
       let minutes = Math.floor(this.remainingSeconds / 60);
       let seconds = this.remainingSeconds % 60;
@@ -57,6 +82,15 @@ export default {
       minutes = minutes % 60;
 
       return `${hours}h ${minutes}m ${seconds}s`;
+    },
+    /**
+     * Message to be shown on the button which controls whether the card is expanded.
+     */
+    expandMessage() {
+      if (this.viewCard) {
+        return 'Hide card';
+      }
+      return 'View card';
     }
   }
 };
