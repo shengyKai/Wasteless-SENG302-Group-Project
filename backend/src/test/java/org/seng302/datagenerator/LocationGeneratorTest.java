040603eb249f3253eb75025b50d3fa12110b953c
Sheng-He Phua <shp55@uclive.ac.nz>
2021-07-10 20:41:00 +1200

    S302T500-235 #time 35m Added more unit tests for the LocationGenerator, now such that all generated field values are checked against the example data used in the LocationGenerator, to see if they exist in the example data. ?test ?document

--- backend/src/test/java/org/seng302/datagenerator/LocationGeneratorTest.java
+++ backend/src/test/java/org/seng302/datagenerator/LocationGeneratorTest.java
@@ -7,18 +7,34 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.lang.reflect.Field;
 import java.sql.*;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes={Main.class})
 public class LocationGeneratorTest {
+
+  private static final String STREET_NAMES_FILE = "street-names.txt";
+  private static final String CITIES_FILE = "cities.txt";
+  private static final String REGIONS_FILE = "regions.txt";
+  private static final String COUNTRIES_FILE = "countries.txt";
+  private static final String DISTRICTS_FILE = "districts.txt";
+
   private Connection conn;
-  // LocationGenerator is called by UserGenerator or BusinessGenerator, so it does not have to be called on its own.
+  // LocationGenerator is called by UserGenerator or BusinessGenerator, so it has to be called with either of them
   private UserGenerator userGenerator;
-  
+
+  //loads all the example files which are the same as the generators
+  private List<String> streetNames = ExampleDataFileReader.readExampleDataFile(STREET_NAMES_FILE);
+  private List<String> cities = ExampleDataFileReader.readExampleDataFile(CITIES_FILE);
+  private List<String> regions = ExampleDataFileReader.readExampleDataFile(REGIONS_FILE);
+  private List<String> countries = ExampleDataFileReader.readExampleDataFile(COUNTRIES_FILE);
+  private List<String> districts = ExampleDataFileReader.readExampleDataFile(DISTRICTS_FILE);
+
   @BeforeEach
   public void setup() throws SQLException {
       //Connects to production database
@@ -83,6 +99,11 @@ public class LocationGeneratorTest {
     }
   }
 
+  /**
+   * Gets the location id from the generated user. 
+   * @param userId id of the inquired user from the table
+   * @return id of the location from the table
+   */
   public Long getLocationIdFromDB(Long userId) throws SQLException {
     PreparedStatement stmt = conn.prepareStatement("SELECT address_id FROM user WHERE userid = ?");
     stmt.setObject(1, userId);
@@ -90,6 +111,21 @@ public class LocationGeneratorTest {
     ResultSet results = stmt.getResultSet();
     results.next();
     return results.getLong(1);
+  }
+
+  /**
+   * Gets the specified field value of the location from the DB to check the field validity.
+   * @param fieldName Specified location field that is required
+   * @param locationId Location Id of the generated location
+   * @return a String containing the field value of the location from the DB
+   */
+  public String getLocationFieldFromDB(String fieldName, Long locationId) throws SQLException {
+    PreparedStatement stmt = conn.prepareStatement("SELECT " + fieldName + " FROM location WHERE id = ?");
+    stmt.setObject(1, locationId);
+    stmt.executeQuery();
+    ResultSet results = stmt.getResultSet();
+    results.next();
+    return results.getString(1);
   }
 
   @Test
@@ -104,4 +140,79 @@ public class LocationGeneratorTest {
       deleteLocationAndUserFromDB(userIds);
   }
 
+  
+  @Test
+  void generateMultipleLocations_generateMultipleUsers_multipleLocationEntriesGenerated() throws SQLException {
+      List<Long> userIds = userGenerator.generateUsers(10);
+      if (userIds.size() != 10) {
+          fail();
+      }
+
+      for (long userId: userIds) {
+        checkRequiredFieldsNotNull(userId);
+      }
+
+      deleteLocationAndUserFromDB(userIds);
+  }
+
+  @Test
+  void generateZeroLocations_generateZeroUsers_zeroLocationEntriesGenerated() throws SQLException {
+      List<Long> userIds = userGenerator.generateUsers(0);
+      if (userIds.size() != 0) {
+          fail();
+      }
+  }
+
+  @Test
+  void generateNegativeLocations_generateNegativeUsers_noLocationEntriesGenerated() throws SQLException {
+      List<Long> userIds = userGenerator.generateUsers(-1);
+      if (userIds.size() != 0) {
+          fail();
+      }
+  }
+
+  @Test
+  void checkCityValidity_checkedAgainstExampleDataFile_cityIsValid() throws SQLException {
+    List<Long> userIds = userGenerator.generateUsers(1);
+    Long locationId = getLocationIdFromDB(userIds.get(0));
+    String cityResult = getLocationFieldFromDB("city", locationId);
+
+    assertTrue((cities.contains(cityResult)));
+  }
+
+  @Test
+  void checkStreetNameValidity_checkedAgainstExampleDataFile_streetNameIsValid() throws SQLException {
+    List<Long> userIds = userGenerator.generateUsers(1);
+    Long locationId = getLocationIdFromDB(userIds.get(0));
+    String streetNameResult = getLocationFieldFromDB("street_name", locationId);
+
+    assertTrue((streetNames.contains(streetNameResult)));
+  }
+
+  @Test
+  void checkRegionValidity_checkedAgainstExampleDataFile_regionIsValid() throws SQLException {
+    List<Long> userIds = userGenerator.generateUsers(1);
+    Long locationId = getLocationIdFromDB(userIds.get(0));
+    String regionResult = getLocationFieldFromDB("region", locationId);
+
+    assertTrue((regions.contains(regionResult)));
+  }
+
+  @Test
+  void checkCountryValidity_checkedAgainstExampleDataFile_countryIsValid() throws SQLException {
+    List<Long> userIds = userGenerator.generateUsers(1);
+    Long locationId = getLocationIdFromDB(userIds.get(0));
+    String countryResult = getLocationFieldFromDB("country", locationId);
+
+    assertTrue((countries.contains(countryResult)));
+  }
+
+  @Test
+  void checkDistrictValidity_checkedAgainstExampleDataFile_districtIsValid() throws SQLException {
+    List<Long> userIds = userGenerator.generateUsers(1);
+    Long locationId = getLocationIdFromDB(userIds.get(0));
+    String districtResult = getLocationFieldFromDB("district", locationId);
+
+    assertTrue((districts.contains(districtResult)));
+  }
 }
