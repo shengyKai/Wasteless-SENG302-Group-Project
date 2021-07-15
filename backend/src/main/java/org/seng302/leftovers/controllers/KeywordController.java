0824888fc9d4e50f6d85506d80e2054ef4609ff1
Nathan Smithies <nts36@uclive.ac.nz>
2021-07-15 20:15:32 +1200

    S302T500-222 #time 12m Implemented POST /keywords endpoint ?implement

--- backend/src/main/java/org/seng302/leftovers/controllers/KeywordController.java
+++ backend/src/main/java/org/seng302/leftovers/controllers/KeywordController.java
@@ -1,15 +1,21 @@
 package org.seng302.leftovers.controllers;
 
 import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.seng302.leftovers.entities.Keyword;
 import org.seng302.leftovers.persistence.KeywordRepository;
 import org.seng302.leftovers.tools.AuthenticationTokenManager;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.servlet.http.HttpServletRequest;
+import java.security.Key;
 
 @RestController
 public class KeywordController {
@@ -20,6 +26,8 @@ public class KeywordController {
     public KeywordController(KeywordRepository keywordRepository) {
         this.keywordRepository = keywordRepository;
     }
+
+
 
     /**
      * REST GET method to retrieve all the global keyword entities
@@ -37,6 +45,34 @@ public class KeywordController {
                 result.add(keyword.constructJSONObject());
             }
             return result;
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            throw e;
+        }
+    }
+
+    /**
+     * REST POST method to add a new keyword entry
+     * @param request The HTTP request
+     * @param keywordInfo Request body to construct keyword from
+     * @return JSONObject with the created keyword id
+     */
+    @PostMapping("/keywords")
+    public JSONObject addKeyword(HttpServletRequest request, @RequestBody JSONObject keywordInfo) {
+        try {
+            String name = keywordInfo.getAsString("name");
+            logger.info("Adding new keyword with name \"{}\"", name);
+            AuthenticationTokenManager.checkAuthenticationToken(request);
+
+            Keyword keyword = new Keyword(name); // Also validates name
+            if (keywordRepository.findByName(name).isPresent()) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Keyword with the given name already exists");
+            }
+
+            keyword = keywordRepository.save(keyword);
+            JSONObject json = new JSONObject();
+            json.put("keywordId", keyword.getID());
+            return json;
         } catch (Exception e) {
             logger.error(e.getMessage());
             throw e;
--- backend/src/main/java/org/seng302/leftovers/persistence/KeywordRepository.java
+++ backend/src/main/java/org/seng302/leftovers/persistence/KeywordRepository.java
@@ -3,14 +3,25 @@ package org.seng302.leftovers.persistence;
 import org.seng302.leftovers.entities.Keyword;
 import org.seng302.leftovers.entities.MarketplaceCard;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface KeywordRepository extends CrudRepository<Keyword, Long> {
 
     List<Keyword> findByOrderByNameAsc();
+
+
+    /**
+     * Finds the keyword for the given name
+     * @param name Name to select keyword by
+     * @return Keyword if present otherwise empty
+     */
+    Optional<Keyword> findByName(@Param("name") String name);
 
     /**
      * Finds all the keywords for the given card
