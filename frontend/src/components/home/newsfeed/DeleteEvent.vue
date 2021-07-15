57a570f5b33af784774a164bb6fea7618d8af9e2
Sheng-He Phua <shp55@uclive.ac.nz>
2021-07-15 22:29:41 +1200

    S302T500-291 #time 10m Deleted unnecessary code and added the finalised title and main body text for the DeleteEvent component. Also added a not yet functional button to close the message. ?implement

--- frontend/src/components/home/newsfeed/DeleteEvent.vue
+++ frontend/src/components/home/newsfeed/DeleteEvent.vue
@@ -3,35 +3,18 @@
     <v-card-title>
       {{ title }}
     </v-card-title>
-    <v-card-subtitle>
-      {{ date }}
-    </v-card-subtitle>
     <v-card-text>
       {{ text }}
     </v-card-text>
-    <v-expand-transition>
-      <v-container v-show="viewCard">
-        <v-row justify="center">
-          <MarketplaceCard :isExpiryEvent="true" :content="card" style="width: 300px"/>
-        </v-row>
-      </v-container>
-    </v-expand-transition>
     <v-card-actions class="justify-center">
-      <v-btn v-if="!delayed" color="primary" @click="delayExpiry">Delay expiry</v-btn>
-      <v-btn color="secondary" @click="viewCard=!viewCard">{{ expandMessage }}</v-btn>
+      <v-btn color="primary" @click="blah">Close</v-btn>
     </v-card-actions>
-    <v-card-text class="justify-center">
-      <div class="error--text" v-if="errorMessage !== undefined">{{ errorMessage }}</div>
-    </v-card-text>
   </div>
 </template>
 
 <script>
-import MarketplaceCard from '@/components/cards/MarketplaceCard';
-
 export default {
-  name: 'ExpiryEvent',
-  components: { MarketplaceCard },
+  name: 'DeleteEvent',
   props: {
     event: {
       type: Object
@@ -39,32 +22,18 @@ export default {
   },
   data() {
     return {
-      viewCard: false,
-      errorMessage: undefined,
-      delayed: false,
+      title: "Your marketplace card has been deleted"
     };
   },
   computed: {
-    /**
-     * Access the card associated with this event.
-     */
-    card() {
-      return this.event.card;
+    text() {
+      return `Your marketplace card "${this.event.title}" in the ${this.section} section has been removed from the marketplace`;
     },
-    /**
-     * Message to be shown on the button which controls whether the card is expanded.
-     */
-    expandMessage() {
-      if (this.viewCard) {
-        return 'Hide card';
+    section() {
+      if (this.event.section === "ForSale") {
+        return "For Sale";
       }
-      return 'View card';
-    },
-    /**
-     * Title of the newsfeed item. Depends on whether the card's expiry has been delayed.
-     */
-    title() {
-      return "deleted Event";
+      return this.event.section;
     }
   },
 };
