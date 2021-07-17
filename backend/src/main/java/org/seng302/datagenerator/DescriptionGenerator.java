60f014d425c23e8505f588ae37a1f9a7d6a732e9
Joshua Egan <jme175@uclive.ac.nz>
2021-07-17 19:16:03 +1200

    S302T500-234 #time 1h 30m Made changes suggested in review for capitalisation and punctuation and min length.  Added tests to ensure passes regex of each class that has a description to be generated with this.  ?fix ?test

--- backend/src/main/java/org/seng302/datagenerator/DescriptionGenerator.java
+++ backend/src/main/java/org/seng302/datagenerator/DescriptionGenerator.java
@@ -6,15 +6,15 @@ public class DescriptionGenerator {
     private static DescriptionGenerator instance;
 
     private static final String DESCRIPTIONS_FILE = "lorem-ipsum.txt";
-    private String descriptions;
+    private final String descriptions;
 
-    private Random random;
+    private final Random random;
 
     /**
      * Instance constructor for description generator, loads file of descriptions to be selected from
      * Description file is all one line so description is String of first in List
      */
-    DescriptionGenerator() {
+    private DescriptionGenerator() {
         descriptions = ExampleDataFileReader.readExampleDataFile(DESCRIPTIONS_FILE).get(0);
         random = new Random();
     }
@@ -25,8 +25,18 @@ public class DescriptionGenerator {
      */
     public String randomDescription() {
         int start = random.nextInt(descriptions.length());
-        int end = start + random.nextInt(199) + 1;  // 200 characters is max length of descriptions
+        int end = start + random.nextInt(190) + 10;  // 10-199 char
         if (end > descriptions.length()) end = descriptions.length();
-        return descriptions.substring(start, end-1);
+        String desc = descriptions.substring(start, end-1).trim() + ".";
+        return desc.substring(0,1).toUpperCase() + desc.substring(1);
+    }
+
+    /**
+     * Description singleton
+     * @return an instance of the description generator class
+     */
+    public static DescriptionGenerator getInstance() {
+        if (instance == null) instance = new DescriptionGenerator();
+        return instance;
     }
 }
--- backend/src/test/java/org/seng302/datagenerator/DescriptionGeneratorTest.java
+++ backend/src/test/java/org/seng302/datagenerator/DescriptionGeneratorTest.java
@@ -1,15 +1,93 @@
 package org.seng302.datagenerator;
 
-import org.junit.Test;
+import org.junit.jupiter.api.*;
+import org.seng302.leftovers.entities.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@SpringBootTest(classes=DescriptionGenerator.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class DescriptionGeneratorTest {
-    DescriptionGenerator descGen = new DescriptionGenerator();
+    DescriptionGenerator descGen = DescriptionGenerator.getInstance();
+    String description;
+
+    User testUser;
+    MarketplaceCard card;
+    Business testBusiness;
+    Product product;
+
+    @BeforeAll
+    void setup() {
+        testUser = new User.Builder()
+                .withFirstName("John")
+                .withMiddleName("Hector")
+                .withLastName("Smith")
+                .withNickName("Jonny")
+                .withEmail("johnsmith98@gmail.com")
+                .withPassword("1337-H%nt3r2")
+                .withBio("Likes long walks on the beach")
+                .withDob("2000-03-11")
+                .withPhoneNumber("+64 3 555 0129")
+                .withAddress(Location.covertAddressStringToLocation("4,Rountree Street,Ashburton,Christchurch,New Zealand," +
+                        "Canterbury,8041"))
+                .build();
+        var closes = Instant.now().plus(1000, ChronoUnit.HOURS);
+        card = new MarketplaceCard.Builder()
+                .withCreator(testUser)
+                .withSection(MarketplaceCard.Section.EXCHANGE)
+                .withTitle("test_title")
+                .withDescription("test_description")
+                .withCloses(closes)
+                .build();
+        testBusiness = new Business.Builder()
+                .withBusinessType("Accommodation and Food Services")
+                .withAddress(Location.covertAddressStringToLocation("4,Rountree Street,Ashburton,Christchurch,New Zealand," +
+                        "Canterbury,8041"))
+                .withDescription("Some description")
+                .withName("BusinessName")
+                .withPrimaryOwner(testUser)
+                .build();
+        product = new Product.Builder()
+                .withProductCode("ORANGE-69")
+                .withName("Fresh Orange")
+                .withDescription("This is a fresh orange")
+                .withManufacturer("Apple")
+                .withRecommendedRetailPrice("2.01")
+                .withBusiness(testBusiness)
+                .build();
+    }
 
     @Test
-    public void randomDescriptionTest() {
-        String description = descGen.randomDescription();
-        assert(description.length() < 200 && description.length() > 0);
+    void randomDescriptionTest() {
+        description = descGen.randomDescription();
+        assert(description.length() < 200 && description.length() >= 10);
+    }
+
+    @Test
+    void marketplaceCardDescValid() {
+        description = descGen.randomDescription();
+        assertDoesNotThrow(()->card.setDescription(description));
+    }
+
+    @Test
+    void userBioValid() {
+        description = descGen.randomDescription();
+        assertDoesNotThrow(()->testUser.setBio(description));
+    }
+
+    @Test
+    void businessDescValid() {
+        description = descGen.randomDescription();
+        assertDoesNotThrow(()->testBusiness.setDescription(description));
+    }
+
+    @Test
+    void productDescValid() {
+        description = descGen.randomDescription();
+        assertDoesNotThrow(()->product.setDescription(description));
     }
 }
