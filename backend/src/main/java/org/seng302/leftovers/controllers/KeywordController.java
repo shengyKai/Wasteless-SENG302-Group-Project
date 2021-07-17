7dbf91a5bf8eff54c3a3ab0f962a0b944e93e92f
Nathan Smithies <nts36@uclive.ac.nz>
2021-07-17 20:19:37 +1200

    S302T500-225 #time 26m Implemented DELETE /keywords/:id endpoint ?implement
    Had to add a @PreRemove handler to Keyword so that deleted keywords are
    removed from the relevant cards

--- backend/src/main/java/org/seng302/leftovers/controllers/KeywordController.java
+++ backend/src/main/java/org/seng302/leftovers/controllers/KeywordController.java
@@ -6,8 +6,12 @@ import org.apache.logging.log4j.Logger;
 import org.seng302.leftovers.entities.Keyword;
 import org.seng302.leftovers.persistence.KeywordRepository;
 import org.seng302.leftovers.tools.AuthenticationTokenManager;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -37,6 +41,27 @@ public class KeywordController {
                 result.add(keyword.constructJSONObject());
             }
             return result;
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            throw e;
+        }
+    }
+
+    @DeleteMapping("/keywords/{id}")
+    public void deleteKeyword(HttpServletRequest request, @PathVariable Long id) {
+        try {
+            logger.info("Deleting keyword: {}", id);
+
+            AuthenticationTokenManager.checkAuthenticationToken(request);
+
+            if (!AuthenticationTokenManager.sessionIsAdmin(request)) {
+                throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Only admin users can delete keywords");
+            }
+
+            Keyword keyword = keywordRepository.findById(id)
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "Keyword not found"));
+            keywordRepository.delete(keyword);
+
         } catch (Exception e) {
             logger.error(e.getMessage());
             throw e;
