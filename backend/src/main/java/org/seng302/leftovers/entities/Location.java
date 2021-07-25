9900e103f51ef0e02e5d94dcaf0b6d089a1637d1
Edward <ezw21@uclive.ac.nz>
2021-07-26 00:08:57 +1200

    S302T500-270 3time 25m Modify backend regex, satisfy the condition now taking ` -'. `. back front synchronized manual testing the register page troughly ?implement ?test

--- backend/src/main/java/org/seng302/leftovers/entities/Location.java
+++ backend/src/main/java/org/seng302/leftovers/entities/Location.java
@@ -141,7 +141,7 @@ public class Location {
      * @return true if the street name is valid, false otherwise
      */
     public boolean checkValidStreetName(String streetName) {
-        if (streetName != null && streetName.length() <= 100 && streetName.length() > 0 && streetName.matches("[ \\p{L}]+")) {
+        if (streetName != null && streetName.length() <= 100 && streetName.length() > 0 && streetName.matches("[ \\p{L}-'.]+")) {
             return true;
         } else {
             return false;
@@ -157,7 +157,7 @@ public class Location {
      * @return true if the city name is valid, false otherwise
      */
     public boolean checkValidCity(String city) {
-        if (city != null && city.length() < 100 && city.length() > 0 && city.matches("[ \\p{L}]+")) {
+        if (city != null && city.length() < 100 && city.length() > 0 && city.matches("[ \\p{L}.'-]+")) {
             return true;
         } else {
             return false;
@@ -173,7 +173,7 @@ public class Location {
      * @return true if the region name is valid, false otherwise
      */
     public boolean checkValidRegion(String region) {
-        if (region != null && region.length() < 100 && region.length() > 0 && region.matches("[ \\p{L}]+")) {
+        if (region != null && region.length() < 100 && region.length() > 0 && region.matches("[ \\p{L}.'-]+")) {
             return true;
         } else {
             return false;
@@ -189,7 +189,7 @@ public class Location {
      * @return true if the country name is valid, false otherwise
      */
     public boolean checkValidCountry(String country) {
-        if (country != null && country.length() < 100 && country.length() > 0 && country.matches("[ \\p{L}]+")) {
+        if (country != null && country.length() < 100 && country.length() > 0 && country.matches("[ \\p{L}.'-]+")) {
             return true;
         } else {
             return false;
@@ -221,7 +221,7 @@ public class Location {
      * @return true if the district is valid, false otherwise
      */
     public boolean checkValidDistrict(String district) {
-        if (district == null || (district.length() <= 100 && district.matches("[\\p{L}0-9.'- ]+"))) {
+        if (district == null || (district.length() <= 100 && district.matches("[\\p{L}0-9.'-]+"))) {
             return true;
         } else {
             return false;
