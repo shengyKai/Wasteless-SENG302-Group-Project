bd295d513439bb76c7a2b1bf5b0fff4bd0080009
Henry Barrett <hrb74@uclive.ac.nz>
2021-07-27 12:39:44 +1200

    S302T500-245 #time 30m continued to work on fixing broken tests ?test

--- backend/src/test/java/org/seng302/leftovers/controllers/SaleControllerTest.java
+++ backend/src/test/java/org/seng302/leftovers/controllers/SaleControllerTest.java
@@ -51,7 +51,7 @@ class SaleControllerTest {
 
     @Mock
     private BusinessRepository businessRepository;
-    @Mock
+    @Spy
     private SaleItemRepository saleItemRepository;
     @Mock
     private InventoryItemRepository inventoryItemRepository;
@@ -355,7 +355,6 @@ class SaleControllerTest {
         for (long i = 0; i<6; i++) {
             SaleItem saleItem = mock(SaleItem.class);
             when(saleItem.getSaleId()).thenReturn(i);
-            when(saleItem.getQuantity()).thenReturn((int) i);
             var json = new JSONObject();
             json.put("id", i);
             when(saleItem.constructJSONObject()).thenReturn(json);
@@ -370,303 +369,102 @@ class SaleControllerTest {
     void getSaleItemsForBusiness_noReverse_itemsAscending() throws Exception {
         var items = generateMockSaleItems();
         when(saleItemRepository.findAllForBusiness(any(Business.class), any())).thenReturn(new PageImpl<SaleItem>(items));
+        MvcResult result = mockMvc.perform(get("/businesses/1/listings"))
+                .andExpect(status().isOk())
+                .andReturn();
+        Sort.Order expectedOrder = new Sort.Order(Sort.Direction.ASC, "created").ignoreCase();
+        PageRequest expectedRequest = SearchHelper.getPageRequest(null,null, Sort.by(expectedOrder));
+        verify(saleItemRepository).findAllForBusiness(any(), eq(expectedRequest));
+    }
+
+    @Test
+    void getSaleItemsForBusiness_reverseFalse_itemsAscending() throws Exception {
+
+        var items = generateMockSaleItems();
+        when(saleItemRepository.findAllForBusiness(any(Business.class), any())).thenReturn(new PageImpl<SaleItem>(items));
+        saleItemRepository.saveAll(items);
+
+        MvcResult result = mockMvc.perform(get("/businesses/1/listings")
+                .param("orderBy", "quantity")
+                .param("reverse", "false"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Sort.Order expectedOrder = new Sort.Order(Sort.Direction.ASC, "created").ignoreCase();
+        PageRequest expectedRequest = SearchHelper.getPageRequest(null,null, Sort.by(expectedOrder));
+        verify(saleItemRepository).findAllForBusiness(any(), eq(expectedRequest));
+    }
+
+    @Test
+    void getSaleItemsForBusiness_reverseTrue_itemsDescending() throws Exception {
+        var items = generateMockSaleItems();
+        when(saleItemRepository.findAllForBusiness(any(Business.class), any())).thenReturn(new PageImpl<SaleItem>(items));
+        saleItemRepository.saveAll(items);
 
 
         MvcResult result = mockMvc.perform(get("/businesses/1/listings")
-                .param("orderBy", "quantity"))
+                .param("orderBy", "quantity")
+                .param("reverse", "true"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Sort.Order expectedOrder = new Sort.Order(Sort.Direction.DESC, "created").ignoreCase();
+        PageRequest expectedRequest = SearchHelper.getPageRequest(null,null, Sort.by(expectedOrder));
+        verify(saleItemRepository).findAllForBusiness(any(), eq(expectedRequest));
+    }
+
+    @Test
+    void getSaleItemsForBusiness_resultsPerPageSet_firstPageReturned() throws Exception {
+        var items = generateMockSaleItems();
+        when(saleItemRepository.findAllForBusiness(any(Business.class), any())).thenCallRealMethod();
+        saleItemRepository.saveAll(items);
+
+
+        MvcResult result = mockMvc.perform(get("/businesses/1/listings")
+                .param("resultsPerPage", "4"))
                 .andExpect(status().isOk())
                 .andReturn();
 
         JSONParser parser = new JSONParser(JSONParser.MODE_PERMISSIVE);
-        Object response = parser.parse(result.getResponse().getContentAsString());
-        JSONObject expectedResult = new JSONObject();
-        expectedResult.appendField("count", 6);
+        JSONObject response = (JSONObject) parser.parse(result.getResponse().getContentAsString());
 
-        JSONArray expectedArray = new JSONArray();
-        for (int i = 0; i<6; i++) {
-            var object = new JSONObject();
-            object.put("id", i);
-            expectedArray.add(object);
-        }
-        expectedResult.appendField("results", expectedArray);
-        assertEquals(expectedResult, response);
+        JSONArray results = (JSONArray) response.get("results");
+        assertEquals(4, results.size());
     }
-//
-//    @Test
-//    void getSaleItemsForBusiness_reverseFalse_itemsAscending() throws Exception {
-//        var items = generateMockSaleItems();
-//        when(saleItemRepository.findAllForBusiness(any(Business.class))).thenReturn(items);
-//        when(saleController.getSaleItemComparator(nullable(String.class)))
-//                .thenReturn(Comparator.comparing(SaleItem::getSaleId));
-//
-//        MvcResult result = mockMvc.perform(get("/businesses/1/listings")
-//                .param("reverse", "false"))
-//                .andExpect(status().isOk())
-//                .andReturn();
-//
-//        JSONParser parser = new JSONParser(JSONParser.MODE_PERMISSIVE);
-//        Object response = parser.parse(result.getResponse().getContentAsString());
-//
-//        JSONArray expected = new JSONArray();
-//        for (int i = 0; i<6; i++) {
-//            var object = new JSONObject();
-//            object.put("id", i);
-//            expected.add(object);
-//        }
-//        assertEquals(expected, response);
-//    }
-//
-//    @Test
-//    void getSaleItemsForBusiness_reverseTrue_itemsDescending() throws Exception {
-//        var items = generateMockSaleItems();
-//        when(saleItemRepository.findAllForBusiness(any(Business.class))).thenReturn(items);
-//        when(saleController.getSaleItemComparator(nullable(String.class)))
-//                .thenReturn(Comparator.comparing(SaleItem::getSaleId));
-//
-//        MvcResult result = mockMvc.perform(get("/businesses/1/listings")
-//                .param("reverse", "true"))
-//                .andExpect(status().isOk())
-//                .andReturn();
-//
-//        JSONParser parser = new JSONParser(JSONParser.MODE_PERMISSIVE);
-//        Object response = parser.parse(result.getResponse().getContentAsString());
-//
-//        JSONArray expected = new JSONArray();
-//        for (int i = 0; i<6; i++) {
-//            var object = new JSONObject();
-//            object.put("id", 5 - i);
-//            expected.add(object);
-//        }
-//        assertEquals(expected, response);
-//    }
-//
-//    @Test
-//    void getSaleItemsForBusiness_resultsPerPageSet_firstPageReturned() throws Exception {
-//        var items = generateMockSaleItems();
-//        when(saleItemRepository.findAllForBusiness(any(Business.class))).thenReturn(items);
-//        when(saleController.getSaleItemComparator(nullable(String.class)))
-//                .thenReturn(Comparator.comparing(SaleItem::getSaleId));
-//
-//        MvcResult result = mockMvc.perform(get("/businesses/1/listings")
-//                .param("resultsPerPage", "4"))
-//                .andExpect(status().isOk())
-//                .andReturn();
-//
-//        JSONParser parser = new JSONParser(JSONParser.MODE_PERMISSIVE);
-//        Object response = parser.parse(result.getResponse().getContentAsString());
-//
-//        JSONArray expected = new JSONArray();
-//        for (int i = 0; i<4; i++) {
-//            var object = new JSONObject();
-//            object.put("id", i);
-//            expected.add(object);
-//        }
-//        assertEquals(expected, response);
-//    }
-//
-//    @Test
-//    void getSaleItemsForBusiness_secondPageRequested_secondPageReturned() throws Exception {
-//        var items = generateMockSaleItems();
-//        when(saleItemRepository.findAllForBusiness(any(Business.class))).thenReturn(items);
-//        when(saleController.getSaleItemComparator(nullable(String.class)))
-//                .thenReturn(Comparator.comparing(SaleItem::getSaleId));
-//
-//        MvcResult result = mockMvc.perform(get("/businesses/1/listings")
-//                .param("resultsPerPage", "4")
-//                .param("page", "2"))
-//                .andExpect(status().isOk())
-//                .andReturn();
-//
-//        JSONParser parser = new JSONParser(JSONParser.MODE_PERMISSIVE);
-//        Object response = parser.parse(result.getResponse().getContentAsString());
-//
-//        JSONArray expected = new JSONArray();
-//        for (int i = 4; i<6; i++) {
-//            var object = new JSONObject();
-//            object.put("id", i);
-//            expected.add(object);
-//        }
-//        assertEquals(expected, response);
-//    }
-//
-//    @Test
-//    void getSaleItemComparator_orderByCreated_comparesCreatedCorrectly(){
-//        SaleItem saleItem1 = mock(SaleItem.class);
-//        SaleItem saleItem2 = mock(SaleItem.class);
-//        SaleItem saleItem3 = mock(SaleItem.class);
-//
-//        Instant now = Instant.now();
-//        when(saleItem1.getCreated()).thenReturn(now);
-//        when(saleItem2.getCreated()).thenReturn(now);
-//        when(saleItem3.getCreated()).thenReturn(now.plusSeconds(1));
-//
-//        var comparator = saleController.getSaleItemComparator("created");
-//        assertEquals(0, comparator.compare(saleItem1, saleItem2));
-//        assertTrue(comparator.compare(saleItem1, saleItem3) < 0);
-//    }
-//
-//    @Test
-//    void getSaleItemComparator_orderByNull_comparesCreatedCorrectly(){
-//        SaleItem saleItem1 = mock(SaleItem.class);
-//        SaleItem saleItem2 = mock(SaleItem.class);
-//        SaleItem saleItem3 = mock(SaleItem.class);
-//
-//        Instant now = Instant.now();
-//        when(saleItem1.getCreated()).thenReturn(now);
-//        when(saleItem2.getCreated()).thenReturn(now);
-//        when(saleItem3.getCreated()).thenReturn(now.plusSeconds(1));
-//
-//        var comparator = saleController.getSaleItemComparator(null);
-//        assertEquals(0, comparator.compare(saleItem1, saleItem2));
-//        assertTrue(comparator.compare(saleItem1, saleItem3) < 0);
-//    }
-//
-//    @Test
-//    void getSaleItemComparator_orderByCloses_comparesClosesCorrectly() {
-//        SaleItem saleItem1 = mock(SaleItem.class);
-//        SaleItem saleItem2 = mock(SaleItem.class);
-//        SaleItem saleItem3 = mock(SaleItem.class);
-//
-//        LocalDate now = LocalDate.now();
-//        when(saleItem1.getCloses()).thenReturn(now);
-//        when(saleItem2.getCloses()).thenReturn(now);
-//        when(saleItem3.getCloses()).thenReturn(now.plusDays(1));
-//
-//        var comparator = saleController.getSaleItemComparator("closing");
-//        assertEquals(0, comparator.compare(saleItem1, saleItem2));
-//        assertTrue(comparator.compare(saleItem1, saleItem3) < 0);
-//    }
-//
-//    @Test
-//    void getSaleItemComparator_orderByProductCode_comparesProductCodeCorrectly() {
-//        SaleItem saleItem1 = mock(SaleItem.class);
-//        SaleItem saleItem2 = mock(SaleItem.class);
-//        SaleItem saleItem3 = mock(SaleItem.class);
-//
-//        Product product1 = mock(Product.class);
-//        Product product2 = mock(Product.class);
-//        Product product3 = mock(Product.class);
-//
-//        when(saleItem1.getProduct()).thenReturn(product1);
-//        when(saleItem2.getProduct()).thenReturn(product2);
-//        when(saleItem3.getProduct()).thenReturn(product3);
-//
-//        when(product1.getProductCode()).thenReturn("AAA");
-//        when(product2.getProductCode()).thenReturn("AAA");
-//        when(product3.getProductCode()).thenReturn("BBB");
-//
-//        var comparator = saleController.getSaleItemComparator("productCode");
-//        assertEquals(0, comparator.compare(saleItem1, saleItem2));
-//        assertTrue(comparator.compare(saleItem1, saleItem3) < 0);
-//    }
-//
-//    @Test
-//    void getSaleItemComparator_orderByProductName_comparesProductNameCorrectly() {
-//        SaleItem saleItem1 = mock(SaleItem.class);
-//        SaleItem saleItem2 = mock(SaleItem.class);
-//        SaleItem saleItem3 = mock(SaleItem.class);
-//
-//        Product product1 = mock(Product.class);
-//        Product product2 = mock(Product.class);
-//        Product product3 = mock(Product.class);
-//
-//        when(saleItem1.getProduct()).thenReturn(product1);
-//        when(saleItem2.getProduct()).thenReturn(product2);
-//        when(saleItem3.getProduct()).thenReturn(product3);
-//
-//        when(product1.getName()).thenReturn("AAA");
-//        when(product2.getName()).thenReturn("AAA");
-//        when(product3.getName()).thenReturn("BBB");
-//
-//        var comparator = saleController.getSaleItemComparator("productName");
-//        assertEquals(0, comparator.compare(saleItem1, saleItem2));
-//        assertTrue(comparator.compare(saleItem1, saleItem3) < 0);
-//    }
-//
-//    @Test
-//    void getSaleItemComparator_orderByQuantity_comparesQuantityCorrectly() {
-//        SaleItem saleItem1 = mock(SaleItem.class);
-//        SaleItem saleItem2 = mock(SaleItem.class);
-//        SaleItem saleItem3 = mock(SaleItem.class);
-//
-//        when(saleItem1.getQuantity()).thenReturn(1);
-//        when(saleItem2.getQuantity()).thenReturn(1);
-//        when(saleItem3.getQuantity()).thenReturn(2);
-//
-//        var comparator = saleController.getSaleItemComparator("quantity");
-//        assertEquals(0, comparator.compare(saleItem1, saleItem2));
-//        assertTrue(comparator.compare(saleItem1, saleItem3) < 0);
-//    }
-//
-//    @Test
-//    void getSaleItemComparator_orderByPrice_comparesPriceCorrectly() throws Exception {
-//        SaleItem saleItem1 = mock(SaleItem.class);
-//        SaleItem saleItem2 = mock(SaleItem.class);
-//        SaleItem saleItem3 = mock(SaleItem.class);
-//
-//        when(saleItem1.getPrice()).thenReturn(new BigDecimal("1.0"));
-//        when(saleItem2.getPrice()).thenReturn(new BigDecimal("1.0"));
-//        when(saleItem3.getPrice()).thenReturn(new BigDecimal("2.0"));
-//
-//        var comparator = saleController.getSaleItemComparator("price");
-//        assertEquals(0, comparator.compare(saleItem1, saleItem2));
-//        assertTrue(comparator.compare(saleItem1, saleItem3) < 0);
-//    }
-//
-//    @Test
-//    void getSaleItemComparator_orderByNonExistant_throws400Exception() {
-//        var exception = assertThrows(ResponseStatusException.class, () -> saleController.getSaleItemComparator("anything"));
-//        assertEquals(HttpStatus.BAD_REQUEST, exception.getStatus());
-//        assertEquals("Invalid sort order", exception.getReason());
-//    }
-//
-//    @Test
-//    void getSalesItemForBusinessCount_noAuthentication_401Response() throws Exception {
-//        // Mock the AuthenticationTokenManager to respond as it would when the authentication token is missing or invalid
-//        authenticationTokenManager.when(() -> AuthenticationTokenManager.checkAuthenticationToken(any()))
-//                .thenThrow(new AccessTokenException());
-//
-//        // Verify that a 401 response is received in response to the GET request
-//        mockMvc.perform(get("/businesses/1/listings/count"))
-//                .andExpect(status().isUnauthorized())
-//                .andReturn();
-//
-//        // Check that the authentication token manager was called
-//        authenticationTokenManager.verify(() -> AuthenticationTokenManager.checkAuthenticationToken(any()));
-//    }
-//
-//    @Test
-//    void getSalesItemForBusinessCount_invalidBusiness_406Response() throws Exception {
-//        // Verify that a 401 response is received in response to the GET request
-//        mockMvc.perform(get("/businesses/999/listings/count"))
-//                .andExpect(status().isNotAcceptable())
-//                .andReturn();
-//    }
-//
-//    @Test
-//    void getSalesItemForBusinessCount_validBusiness_doesNotCheckSessionPermissions() throws Exception {
-//        mockMvc.perform(get("/businesses/1/listings/count"))
-//                .andReturn();
-//
-//        verify(business, times(0)).checkSessionPermissions(any(HttpServletRequest.class));
-//    }
-//
-//    @Test
-//    void getSalesItemForBusinessCount_validBusinessWithSalesItems_returnsSalesItemCount() throws Exception {
-//        @SuppressWarnings("unchecked")
-//        List<SaleItem> saleItems = (List<SaleItem>)mock(List.class);
-//
-//        when(saleItemRepository.findAllForBusiness(any(Business.class))).thenReturn(saleItems);
-//        when(saleItems.size()).thenReturn(500);
-//
-//        MvcResult result = mockMvc.perform(get("/businesses/1/listings/count"))
-//                .andReturn();
-//        JSONParser parser = new JSONParser(JSONParser.MODE_PERMISSIVE);
-//        Object response = parser.parse(result.getResponse().getContentAsString());
-//
-//        var expected = new JSONObject();
-//        expected.put("count", 500);
-//
-//        assertEquals(expected, response);
-//    }
+
+    @Test
+    void getSaleItemsForBusiness_secondPageRequested_secondPageReturned() throws Exception {
+        var items = generateMockSaleItems();
+        when(saleItemRepository.findAllForBusiness(any(Business.class), any())).thenCallRealMethod();
+        saleItemRepository.saveAll(items);
+
+
+        MvcResult result = mockMvc.perform(get("/businesses/1/listings")
+                .param("resultsPerPage", "4")
+                .param("page","2"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        JSONParser parser = new JSONParser(JSONParser.MODE_PERMISSIVE);
+        JSONObject response = (JSONObject) parser.parse(result.getResponse().getContentAsString());
+
+        JSONArray results = (JSONArray) response.get("results");
+        assertEquals(2, results.size());
+    }
+
+    @Test
+    void getSaleItemsForBusiness_count() throws Exception {
+        var items = generateMockSaleItems();
+        saleItemRepository.saveAll(items);
+
+
+        MvcResult result = mockMvc.perform(get("/businesses/1/listings"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        JSONParser parser = new JSONParser(JSONParser.MODE_PERMISSIVE);
+        JSONObject response = (JSONObject) parser.parse(result.getResponse().getContentAsString());
+
+        assertEquals(2, response.getAsNumber("count").intValue());
+    }
 }
