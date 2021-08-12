d8a10fdddfd6da0029bc6a9338ddcf09643999da
Ella Johnson <egj21@uclive.ac.nz>
2021-08-13 11:40:38 +1200

    S302T500-333 #time 30m Restructured MesssageEvent entity to try to add uniqueness constraint for conversation and participant. Uniqueness constraint works now but there is some redundancy in the entity ?implement

--- backend/src/main/java/org/seng302/leftovers/entities/MessageEvent.java
+++ backend/src/main/java/org/seng302/leftovers/entities/MessageEvent.java
@@ -1,19 +1,42 @@
 package org.seng302.leftovers.entities;
 
 import net.minidev.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.persistence.*;
 
 @Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"conversation_id", "participant_type"}))
 public class MessageEvent extends Event {
 
     @ManyToOne
     @JoinColumn(name = "message_id", nullable = false)
     private Message message;
 
+    @ManyToOne
+    @JoinColumn(name = "conversation_id", nullable = false)
+    private Conversation conversation;
+
+    @Column(name = "participant_type")
+    private ParticipantType participantType;
+
+    private enum ParticipantType {
+        BUYER,
+        SELLER
+    }
+
     public MessageEvent(User notifiedUser, Message message) {
         super(notifiedUser);
+        if (message.getConversation().getBuyer().equals(notifiedUser)) {
+            this.participantType = ParticipantType.BUYER;
+        } else if (message.getConversation().getCard().getCreator().equals(notifiedUser)) {
+            this.participantType = ParticipantType.SELLER;
+        } else {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Notification can only be sent to buyer or seller of card");
+        }
         this.message = message;
+        this.conversation = message.getConversation();
     }
 
     protected MessageEvent() {
@@ -24,14 +47,23 @@ public class MessageEvent extends Event {
         return message;
     }
 
+    public ParticipantType getParticipantType() {
+        return participantType;
+    }
+
+    public Conversation getConversation() {
+        return conversation;
+    }
+
     public void setMessage(Message message) {
         this.message = message;
     }
 
+    @Override
     public JSONObject constructJSONObject() {
         JSONObject jsonObject = super.constructJSONObject();
         jsonObject.appendField("message", message.constructJSONObject());
-        jsonObject.appendField("card", message.getConversation().getCard().constructJSONObject());
+        jsonObject.appendField("card", conversation.getCard().constructJSONObject());
         return jsonObject;
     }
 }
