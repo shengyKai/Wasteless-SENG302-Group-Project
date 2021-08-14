ab5904b51900a4b66b7b9d3a3181c29fbe1ac6b9
Ella Johnson <egj21@uclive.ac.nz>
2021-08-14 12:06:28 +1200

    S302T500-321 #time 10m Fixed errors resulting from merge conflict ?chore

--- frontend/src/components/home/newsfeed/Event.vue
+++ frontend/src/components/home/newsfeed/Event.vue
@@ -15,163 +15,161 @@
   </div>
   <div v-else>
     <div class="delete">
-  <div>
-    <div class="float=right ma-1">
-      <v-icon class="deleteButton"
-              ref="deleteButton"
-              color="red"
-              @click.stop="initiateDeletion"
+      <div class="float=right ma-1">
+        <v-icon class="deleteButton"
+                ref="deleteButton"
+                color="red"
+                @click.stop="initiateDeletion"
+        >
+          mdi-trash-can
+        </v-icon>
+      </div>
+      <v-card-title>
+        {{ title }}
+      </v-card-title>
+      <v-card-subtitle>
+        {{ date }}, {{ time }}
+      </v-card-subtitle>
+      <slot/>
+      <v-card-text class="justify-center">
+        <div class="error--text" v-if="errorMessage !== undefined">{{ errorMessage }}</div>
+      </v-card-text>
+      <!-- For user to view their option about the available tag to choose from  -->
+      <v-row
+        justify="start"
+        style="min-height: 10px;"
       >
-        mdi-trash-can
-      </v-icon>
-    </div>
-    <v-card-title>
-      {{ title }}
-    </v-card-title>
-    <v-card-subtitle>
-      {{ date }}, {{ time }}
-    </v-card-subtitle>
-    <slot/>
-    <v-card-text class="justify-center">
-      <div class="error--text" v-if="errorMessage !== undefined">{{ errorMessage }}</div>
-    </v-card-text>
-    <!-- For user to view their option about the available tag to choose from  -->
-    <v-row
-      justify="start"
-      style="min-height: 10px;"
-    >
-      <v-col cols="10">
-        <!-- The persistent chip that shows the tag for the message (default will be no colour) -->
-        <v-tooltip bottom>
-          <template v-slot:activator="{ on, attrs }">
-            <v-chip
-              class="ma-2 ml-4 mb-3"
-              :color="event.tag"
-              @click="expand = !expand"
-              label
-              text-color="white"
-              v-bind="attrs"
-              v-on="on"
-            >
-              <v-icon left>
-                mdi-label
-              </v-icon>
-              Tag
-            </v-chip>
-          </template>
-          <span>Click to view the available tags.</span>
-        </v-tooltip>
-        <v-expand-transition>
-          <v-card
-            v-show="expand"
-            height="70"
-            width="1000"
-            class="mx-auto"
-          >
-            <div class="font-weight-medium">
-              <span class="ml-4">
-                Change your Tag:
-              </span>
-            </div>
-            <!--  Content that run through a loop of colours which at the same time set the colour of the chip
-              Make the code more maintainable as it will be easy to modify colour in future and get the index
-              Trigger a tagNotification when the chip is clicked (will take the colour as param)
-        -->
-            <div class="ml-3">
+        <v-col cols="10">
+          <!-- The persistent chip that shows the tag for the message (default will be no colour) -->
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on, attrs }">
               <v-chip
-                class="ma-1"
-                v-for="colour in colours"
-                :key=colour
-                :color="colour"
+                class="ma-2 ml-4 mb-3"
+                :color="event.tag"
+                @click="expand = !expand"
                 label
                 text-color="white"
-                @click="tagNotification(colour)"
+                v-bind="attrs"
+                v-on="on"
               >
                 <v-icon left>
                   mdi-label
                 </v-icon>
                 Tag
               </v-chip>
-            </div>
-          </v-card>
-        </v-expand-transition>
-      </v-col>
-      <v-col class="float=right">
-        <!-- Tooltip for the reply button telling user what they can do with this button -->
-        <v-tooltip bottom >
-          <template v-slot:activator="{on, attrs }">
-            <v-icon v-if="!isCardOwner"
-                    ref="messageButton"
-                    color="primary"
-                    @click.stop="messageOwnerDialog = true; directMessageContent=''"
-                    v-bind="attrs"
-                    v-on="on"
-                    class="mt-4 ml-12"
+            </template>
+            <span>Click to view the available tags.</span>
+          </v-tooltip>
+          <v-expand-transition>
+            <v-card
+              v-show="expand"
+              height="70"
+              width="1000"
+              class="mx-auto"
             >
-              mdi-reply
-            </v-icon>
-          </template>
-          Reply to this message
-        </v-tooltip>
-      </v-col>
-    </v-row>
-    <!-- Dialog that stay consistent with the firstMessage(in primary colour), replyMessage(in secondary colour) -->
-    <v-dialog ref="messageDialog"
-              v-model="messageOwnerDialog"
-              max-width="600px">
-      <v-card>
-        <!-- The 'TITLE' of the replyMessage component -->
-        <v-card color='secondary lighten-2'>
-          <v-card-title>
-            Send a message to {{firstName}}
-          </v-card-title>
-          <v-card-subtitle>
-            Your message will appear on their feed
-          </v-card-subtitle>
+              <div class="font-weight-medium">
+                <span class="ml-4">
+                  Change your Tag:
+                </span>
+              </div>
+              <!--  Content that run through a loop of colours which at the same time set the colour of the chip
+              Make the code more maintainable as it will be easy to modify colour in future and get the index
+              Trigger a tagNotification when the chip is clicked (will take the colour as param)
+        -->
+              <div class="ml-3">
+                <v-chip
+                  class="ma-1"
+                  v-for="colour in colours"
+                  :key=colour
+                  :color="colour"
+                  label
+                  text-color="white"
+                  @click="tagNotification(colour)"
+                >
+                  <v-icon left>
+                    mdi-label
+                  </v-icon>
+                  Tag
+                </v-chip>
+              </div>
+            </v-card>
+          </v-expand-transition>
+        </v-col>
+        <v-col class="float=right">
+          <!-- Tooltip for the reply button telling user what they can do with this button -->
+          <v-tooltip bottom >
+            <template v-slot:activator="{on, attrs }">
+              <v-icon v-if="!isCardOwner"
+                      ref="messageButton"
+                      color="primary"
+                      @click.stop="messageOwnerDialog = true; directMessageContent=''"
+                      v-bind="attrs"
+                      v-on="on"
+                      class="mt-4 ml-12"
+              >
+                mdi-reply
+              </v-icon>
+            </template>
+            Reply to this message
+          </v-tooltip>
+        </v-col>
+      </v-row>
+      <!-- Dialog that stay consistent with the firstMessage(in primary colour), replyMessage(in secondary colour) -->
+      <v-dialog ref="messageDialog"
+                v-model="messageOwnerDialog"
+                max-width="600px">
+        <v-card>
+          <!-- The 'TITLE' of the replyMessage component -->
+          <v-card color='secondary lighten-2'>
+            <v-card-title>
+              Send a message to {{firstName}}
+            </v-card-title>
+            <v-card-subtitle>
+              Your message will appear on their feed
+            </v-card-subtitle>
+          </v-card>
+          <!-- The Message body input component -->
+          <v-form v-model="directMessageValid" ref="directMessageForm">
+            <v-card-text>
+              <v-textarea
+                solo
+                outlined
+                clearable
+                prepend-inner-icon="mdi-comment"
+                no-resize
+                :counter="200"
+                :rules="mandatoryRules.concat(maxCharRules())"
+                v-model="directMessageContent"/>
+            </v-card-text>
+            <!-- Submit and Cancel button for the replyMessage component -->
+            <v-card-actions>
+              <v-alert v-if="directMessageError !== undefined" color="red" type="error" dense text>
+                {{directMessageError}}
+              </v-alert>
+              <v-spacer/>
+              <v-btn color="primary"
+                     text
+                     :disabled="!directMessageValid"
+                     @click="sendMessage">
+                Send
+              </v-btn>
+              <v-btn color="primary"
+                     text
+                     @click="messageOwnerDialog = false; directMessageError = undefined; directMessageContent=''">
+                Cancel
+              </v-btn>
+            </v-card-actions>
+          </v-form>
         </v-card>
-        <!-- The Message body input component -->
-        <v-form v-model="directMessageValid" ref="directMessageForm">
-          <v-card-text>
-            <v-textarea
-              solo
-              outlined
-              clearable
-              prepend-inner-icon="mdi-comment"
-              no-resize
-              :counter="200"
-              :rules="mandatoryRules.concat(maxCharRules())"
-              v-model="directMessageContent"/>
-          </v-card-text>
-          <!-- Submit and Cancel button for the replyMessage component -->
-          <v-card-actions>
-            <v-alert v-if="directMessageError !== undefined" color="red" type="error" dense text>
-              {{directMessageError}}
-            </v-alert>
-            <v-spacer/>
-            <v-btn color="primary"
-                   text
-                   :disabled="!directMessageValid"
-                   @click="sendMessage">
-              Send
-            </v-btn>
-            <v-btn color="primary"
-                   text
-                   @click="messageOwnerDialog = false; directMessageError = undefined; directMessageContent=''">
-              Cancel
-            </v-btn>
-          </v-card-actions>
-        </v-form>
-      </v-card>
-    </v-dialog>
+      </v-dialog>
+    </div>
   </div>
 </template>
 
 <script>
-import { formatDate } from '@/utils';
 import synchronizedTime from '@/components/utils/Methods/synchronizedTime';
 import { formatDate, maxCharRules, mandatoryRules, } from '@/utils';
-import {deleteNotification, messageConversation} from "@/api/internal";
-import { setEventTag } from '../../../api/internal';
+import { setEventTag, messageConversation} from "@/api/internal";
 
 export default {
   name: 'Event',
