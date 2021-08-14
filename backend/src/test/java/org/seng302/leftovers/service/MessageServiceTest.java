e911c6e2c795ae398c79d9b94b02946676427074
Ella Johnson <egj21@uclive.ac.nz>
2021-08-14 16:58:52 +1200

    S302T500-333 #time 30m Wrote tests for MessageService class ?test

--- backend/src/test/java/org/seng302/leftovers/service/MessageServiceTest.java
+++ backend/src/test/java/org/seng302/leftovers/service/MessageServiceTest.java
@@ -1,12 +1,11 @@
 package org.seng302.leftovers.service;
 
 import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 import org.seng302.leftovers.entities.Conversation;
+import org.seng302.leftovers.entities.MarketplaceCard;
 import org.seng302.leftovers.entities.Message;
 import org.seng302.leftovers.entities.MessageEvent;
 import org.seng302.leftovers.entities.User;
@@ -15,10 +14,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -37,53 +40,87 @@ class MessageServiceTest {
     @Mock
     Conversation otherConversation;
     @Mock
-    Message validMessage;
+    Message firstMessage;
     @Mock
-    Message invalidMessage;
+    Message secondMessage;
+    @Mock
+    Message otherConversationMessage;
     @Mock
     User buyer;
     @Mock
     User seller;
     @Mock
     User bystander;
+    @Mock
+    MarketplaceCard card;
 
-    @Before
+    MessageEvent buyerMessageEvent;
+    MessageEvent sellerMessageEvent;
+
+    @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
 
-        when(validMessage.getConversation()).thenReturn(eventConversation);
-        when(invalidMessage.getConversation()).thenReturn(otherConversation);
+        when(firstMessage.getConversation()).thenReturn(eventConversation);
+        when(secondMessage.getConversation()).thenReturn(eventConversation);
+        when(otherConversationMessage.getConversation()).thenReturn(otherConversation);
 
+        when(buyer.getUserID()).thenReturn(1L);
+        when(seller.getUserID()).thenReturn(2L);
+        when(bystander.getUserID()).thenReturn(3L);
+
+        when(eventConversation.getId()).thenReturn(4L);
+        when(eventConversation.getBuyer()).thenReturn(buyer);
+        when(eventConversation.getCard()).thenReturn(card);
+        when(card.getCreator()).thenReturn(seller);
+
+        buyerMessageEvent = new MessageEvent(buyer, firstMessage);
+        sellerMessageEvent = new MessageEvent(seller, firstMessage);
     }
 
     @Test
-    void notifyConversationParticipants_newConversation_messageSentToBuyer() {
-        fail("Not yet implemented");
+    void notifyConversationParticipants_newConversation_messagesSentToParticipants() {
+        when(messageEventRepository.findByNotifiedUserAndConversation(buyer, eventConversation)).thenReturn(Optional.empty());
+        when(messageEventRepository.findByNotifiedUserAndConversation(seller, eventConversation)).thenReturn(Optional.empty());
+
+        messageService.notifyConversationParticipants(firstMessage, buyer, seller);
+
+        Mockito.verify(eventService, times(2)).saveEvent(messageEventArgumentCaptor.capture());
+
+        List<MessageEvent> capturedEvents = messageEventArgumentCaptor.getAllValues();
+        Set<User> notifiedUsers = capturedEvents.stream().map(MessageEvent::getNotifiedUser).collect(Collectors.toSet());
+        assertTrue(notifiedUsers.contains(buyer));
+        assertTrue(notifiedUsers.contains(seller));
     }
 
     @Test
-    void notifyConversationParticipants_newConversation_messageSentToSeller() {
-        fail("Not yet implemented");
+    void notifyConversationParticipants_existingConversation_messagesSentToParticipants() {
+        when(messageEventRepository.findByNotifiedUserAndConversation(buyer, eventConversation)).thenReturn(Optional.of(buyerMessageEvent));
+        when(messageEventRepository.findByNotifiedUserAndConversation(seller, eventConversation)).thenReturn(Optional.of(sellerMessageEvent));
+
+        messageService.notifyConversationParticipants(secondMessage, buyer, seller);
+
+        Mockito.verify(eventService, times(2)).saveEvent(messageEventArgumentCaptor.capture());
+
+        List<MessageEvent> capturedEvents = messageEventArgumentCaptor.getAllValues();
+        Set<User> notifiedUsers = capturedEvents.stream().map(MessageEvent::getNotifiedUser).collect(Collectors.toSet());
+        assertTrue(notifiedUsers.contains(buyer));
+        assertTrue(notifiedUsers.contains(seller));
     }
 
     @Test
-    void notifyConversationParticipants_existingConversation_buyerMessageUpdated() {
-        fail("Not yet implemented");
-    }
+    void notifyConversationParticipants_invalidParticipant_eventNotSent() {
+        when(messageEventRepository.findByNotifiedUserAndConversation(buyer, eventConversation)).thenReturn(Optional.empty());
+        when(messageEventRepository.findByNotifiedUserAndConversation(seller, eventConversation)).thenReturn(Optional.empty());
 
-    @Test
-    void notifyConversationParticipants_existingConversation_sellerMessageUpdated() {
-        fail("Not yet implemented");
-    }
+        messageService.notifyConversationParticipants(firstMessage, bystander, seller);
 
-    @Test
-    void notifyConversationParticipants_messageEventConstructorThrowsException_eventNotSent() {
-        fail("Not yet implemented");
-    }
+        Mockito.verify(eventService, times(1)).saveEvent(messageEventArgumentCaptor.capture());
 
-    @Test
-    void notifyConversationParticipants_messageEventSetMessageThrowsException_eventNotSent() {
-        fail("Not yet implemented");
+        List<MessageEvent> capturedEvents = messageEventArgumentCaptor.getAllValues();
+        Set<User> notifiedUsers = capturedEvents.stream().map(MessageEvent::getNotifiedUser).collect(Collectors.toSet());
+        assertFalse(notifiedUsers.contains(bystander));
+        assertTrue(notifiedUsers.contains(seller));
     }
 
 }
