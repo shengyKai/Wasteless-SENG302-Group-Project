7dff3f8ca28999dd9e95b8af6d8ddd3fe431a1d2
Connor <fch58@uclive.ac.nz>
2021-08-17 12:53:53 +1200

    S302T500-349 #time 45m Created a wide range of edge cases and blue sky of 48 empty JUnit tests for the modify user API endpoint for TDD ?test

---
+++ backend/src/test/java/org/seng302/leftovers/controllers/UserControllerModifyTest.java
@@ -0,0 +1,264 @@
+package org.seng302.leftovers.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerModifyTest {
+
+    @Test
+    void modifyUser_modifyWithValidFirstName_modifiedUser200() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithValidLastName_modifiedUser200() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithValidMiddleName_modifiedUser200() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithValidNickname_modifiedUser200() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithValidBio_modifiedUser200() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithValidDateOfBirth_modifiedUser200() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithValidPhoneNumber_modifiedUser200() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithValidStreetNumber_modifiedUser200() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithValidStreetName_modifiedUser200() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithValidDistrict_modifiedUser200() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithValidCity_modifiedUser200() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithValidRegion_modifiedUser200() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithValidCountry_modifiedUser200() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithValidPostcode_modifiedUser200() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithValidEmailAndPasswords_modifiedUser200() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithOnlyValidEmail_notModifiedUser403() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithOnlyValidCurrentPassword_notModifiedUser403() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithOnlyValidNewPassword_notModifiedUser403() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithNullMiddleName_userModified200() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithNullNickname_userModified200() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithNullBio_userModified200() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithNullPhoneNumber_userModified200() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithNullDistrict_userModified200() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithNullEmailAndPassword_userNotModified400() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithNullFirstName_userNotModified400() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithNullLastName_userNotModified400() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithNullDateOfBirth_userNotModified400() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithNullStreetAddress_userNotModified400() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithNullCity_userNotModified400() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithNullRegion_userNotModified400() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithNullCountry_userNotModified400() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyWithNullPostcode_userNotModified400() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyEmailTooLong_userNotModified400() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyTooLongPassword_userNotModified400() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyTooLongFirstName_userNotModified400() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyTooLongMiddleName_userNotModified400() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyTooLongLastName_userNotModified400() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyTooLongNickname_userNotModified400() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyTooLongBio_userNotModified400() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyTooLongDateOfBirth_userNotModified400() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyTooShortDateOfBirth_userNotModified400() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyTooLongPhoneNumber_userNotModified400() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyTooLongStreetAddress_userNotModified400() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyTooLongDistrict_userNotModified400() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyTooLongCity_userNotModified400() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyTooLongRegion_userNotModified400() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyTooLongCountry_userNotModified400() throws Exception {
+
+    }
+
+    @Test
+    void modifyUser_modifyTooLongPostcode_userNotModified400() throws Exception {
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
