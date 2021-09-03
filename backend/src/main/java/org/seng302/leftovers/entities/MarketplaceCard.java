881cbccca64fd31e5d9cf09396b0a3316e8a9993
Sheng-He Phua <shp55@uclive.ac.nz>
2021-09-03 19:30:14 +1200

    S302T500-464 #time 1m removed the changes from manual testing because i forgot to change them back and it caused some tests to fail. ?fix

--- backend/src/main/java/org/seng302/leftovers/entities/MarketplaceCard.java
+++ backend/src/main/java/org/seng302/leftovers/entities/MarketplaceCard.java
@@ -414,7 +414,7 @@ public class MarketplaceCard {
             card.created = Instant.now();
             card.lastRenewed = card.created;
             if (closes == null) {
-                card.setCloses(card.created.plus(Duration.ofSeconds(20)));
+                card.setCloses(card.created.plus(DISPLAY_PERIOD));
             } else {
                 card.setCloses(closes);
             }
