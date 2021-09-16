2e46e774af5868a3b5ae4cfaaa87660622ad12e7
Ella Johnson <egj21@uclive.ac.nz>
2021-09-16 15:02:31 +1200

    S302T500-423 #time 30m Wrote Cucumber scenarios in feature file and added method signitures for step definitions ?test

---
+++ backend/src/test/java/cucumber/stepDefinitions/PurchaseStepDefinition.java
@@ -0,0 +1,45 @@
+package cucumber.stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class PurchaseStepDefinition {
+
+    @Given("user {string} has purchased the sale listing {string} from business {string}")
+    public void user_has_purchased_the_sale_listing_from_business(String string, String string2, String string3) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the remaining quantity of the inventory item {string} will be {int}")
+    public void the_remaining_quantity_of_the_inventory_item_will_be(String string, Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I am viewing the sale listings for business {string}")
+    public void i_am_viewing_the_sale_listings_for_business(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the item {string} is not present")
+    public void the_item_is_not_present(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("A record of the purchase is added to the business's sale history")
+    public void a_record_of_the_purchase_is_added_to_the_business_s_sale_history() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I try to purchase the most recent sale listing")
+    public void i_try_to_purchase_the_most_recent_sale_listing() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
