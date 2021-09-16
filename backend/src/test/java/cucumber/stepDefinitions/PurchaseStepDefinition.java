76b19f6cac9b50d1a735e61135c328a41dd74cfc
Ella Johnson <egj21@uclive.ac.nz>
2021-09-16 15:25:58 +1200

    S302T500-423 #time 20m Started writing step definitions ?test

--- backend/src/test/java/cucumber/stepDefinitions/PurchaseStepDefinition.java
+++ backend/src/test/java/cucumber/stepDefinitions/PurchaseStepDefinition.java
@@ -1,21 +1,51 @@
 package cucumber.stepDefinitions;
 
+import cucumber.context.BusinessContext;
+import cucumber.context.RequestContext;
+import cucumber.context.UserContext;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
+import org.seng302.leftovers.persistence.InventoryItemRepository;
+import org.seng302.leftovers.persistence.SaleItemRepository;
+import org.seng302.leftovers.tools.SearchHelper;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.MediaType;
+
+import java.util.stream.Collectors;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 
 public class PurchaseStepDefinition {
 
+    private UserContext userContext;
+    private BusinessContext businessContext;
+    private SaleItemRepository saleItemRepository;
+    private InventoryItemRepository inventoryItemRepository;
+    private RequestContext requestContext;
+
     @Given("user {string} has purchased the sale listing {string} from business {string}")
-    public void user_has_purchased_the_sale_listing_from_business(String string, String string2, String string3) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void user_has_purchased_the_sale_listing_from_business(String userName, String itemName, String businessName) throws JSONException {
+        var user = userContext.getByName(userName);
+        var business = businessContext.getByName(businessName);
+        var pageRequest = PageRequest.of(1, Math.toIntExact(saleItemRepository.count()));
+        var saleItem = saleItemRepository.findAllForBusiness(business, pageRequest).stream()
+                .filter(i -> i.getProduct().getName().equals(itemName)).collect(Collectors.toList()).get(0);
+        var requestBody = new JSONObject();
+        requestBody.put("purchaserId", user.getUserID());
+        requestContext.performRequest(put(String.format("listings/%d/purchase", saleItem.getId()))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody.toString()));
     }
 
     @Then("the remaining quantity of the inventory item {string} will be {int}")
-    public void the_remaining_quantity_of_the_inventory_item_will_be(String string, Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void the_remaining_quantity_of_the_inventory_item_will_be(String itemName, Integer remainingQuantity) {
+        var inventoryItem = inventoryItemRepository.findAllForBusiness(businessContext.getLast()).stream()
+                .filter(i -> i.getProduct().getName().equals(itemName)).collect(Collectors.toList()).get(0);
+        Assertions.assertEquals(remainingQuantity, inventoryItem.getQuantity());
     }
 
     @When("I am viewing the sale listings for business {string}")
