1cc53c7b56536fbf9d0f70065d3de3dbdb842ae9
Henry Barrett <hrb74@uclive.ac.nz>
2021-09-18 18:02:57 +1200

    S302T500-426 #time 10m fixed issue 79. Business search now includes address fields. Commented out the Event until a branch I require has been merged ?chore

--- backend/src/main/java/org/seng302/leftovers/entities/event/PurchasedEvent.java
+++ backend/src/main/java/org/seng302/leftovers/entities/event/PurchasedEvent.java
@@ -1,32 +1,32 @@
-package org.seng302.leftovers.entities.event;
-
-import org.seng302.leftovers.dto.event.EventDTO;
-import org.seng302.leftovers.entities.User;
-
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-
-@Entity
-public class PurchasedEvent extends Event{
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="brought_sale_item_id", nullable = false)
-    private BroughtSaleItem broughtSaleItem;
-
-    public PurchasedEvent(User purchaser, BroughtSaleItem broughtSaleItem) {
-        super(purchaser);
-        this.broughtSaleItem = broughtSaleItem;
-    }
-
-    public BroughtSaleItem getBroughtSaleItem() {
-        return broughtSaleItem;
-    }
-
-    public PurchasedEvent(){}
-
-    @Override
-    public EventDTO asDTO() {
-        return null;
-    }
-}
+//package org.seng302.leftovers.entities.event;
+//
+//import org.seng302.leftovers.dto.event.EventDTO;
+//import org.seng302.leftovers.entities.User;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.FetchType;
+//import javax.persistence.JoinColumn;
+//import javax.persistence.ManyToOne;
+//
+//@Entity
+//public class PurchasedEvent extends Event{
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name="brought_sale_item_id", nullable = false)
+//    private BroughtSaleItem broughtSaleItem;
+//
+//    public PurchasedEvent(User purchaser, BroughtSaleItem broughtSaleItem) {
+//        super(purchaser);
+//        this.broughtSaleItem = broughtSaleItem;
+//    }
+//
+//    public BroughtSaleItem getBroughtSaleItem() {
+//        return broughtSaleItem;
+//    }
+//
+//    public PurchasedEvent(){}
+//
+//    @Override
+//    public EventDTO asDTO() {
+//        return null;
+//    }
+//}
