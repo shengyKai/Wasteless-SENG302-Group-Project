ed1edd471f67bfdf3aa89ac3a65bb2bb30f556a8
hrb74 <shp55@uclive.ac.nz>
2021-09-20 18:13:09 +1200

    S302T500-425 #time 10m Finished on unit the test for the InterestPurchasedEvent. ?test

--- backend/src/test/java/org/seng302/leftovers/entities/event/InterestPurchasedEventTest.java
+++ backend/src/test/java/org/seng302/leftovers/entities/event/InterestPurchasedEventTest.java
@@ -1,14 +1,19 @@
 package org.seng302.leftovers.entities.event;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import net.minidev.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.seng302.leftovers.dto.saleitem.BoughtSaleItemDTO;
 import org.seng302.leftovers.entities.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -38,5 +43,25 @@ public class InterestPurchasedEventTest {
         when(product.getBusiness()).thenReturn(business);
         when(business.getAddress()).thenReturn(businessAddress);
         when(business.getPrimaryOwner()).thenReturn(businessPrimaryOwner);
+    }
+
+    @Test
+    void asDTO_correctJson() {
+        var event = spy(new InterestPurchasedEvent(user, boughtSaleItem));
+        when(event.getId()).thenReturn(30L);
+
+        var json = mapper.convertValue(event.asDTO(), JSONObject.class);
+        assertEquals(event.getId(), json.get("id"));
+        assertEquals(event.getCreated().toString(), json.get("created"));
+        assertEquals("InterestPurchasedEvent", json.get("type"));
+        assertEquals("none", json.get("tag"));
+        assertEquals(event.getStatus().toString().toLowerCase(), json.get("status"));
+        assertEquals(event.isRead(), json.get("read"));
+
+        var actualBoughtSaleItem = mapper.convertValue(json.get("boughtSaleItem"), BoughtSaleItemDTO.class);
+        assertEquals(null, actualBoughtSaleItem.getBuyer());
+        assertEquals(event.getBoughtSaleItem().getId(), actualBoughtSaleItem.getId());
+        assertEquals(event.getLastModified().toString(), json.getAsString("lastModified"));
+        assertEquals(8, json.size());
     }
 }
