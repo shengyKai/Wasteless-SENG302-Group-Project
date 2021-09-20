6088e39e5c44fc6f1fbac9a655fa8f39eca52b07
edward <ezw21@uclive.ac.nz>
2021-09-20 16:43:03 +1200

    S302T500-431 #time 40m Try expand and carousel UI componenet ?implement ?pair[ezw21,fch58]

---
+++ frontend/src/components/utils/ImageManager.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <v-row>
+      <v-carousel v-model="model">
+        <v-carousel-item
+          v-for="(color, i) in colors"
+          :key="color"
+        >
+          <v-sheet
+            :color="color"
+            height="100%"
+            tile
+          >
+            <v-row
+              class="fill-height"
+              align="center"
+              justify="center"
+            >
+              <div class="text-h2">
+                Slide {{ i + 1 }}
+              </div>
+            </v-row>
+          </v-sheet>
+        </v-carousel-item>
+      </v-carousel>
+    </v-row>
+    <v-row no-gutters>
+      <v-col>
+        <v-expand-transition>
+          <v-card
+            v-show="expand"
+            max-width="1200"
+            class="mx-auto"
+          >
+            <div class="font-weight-medium">
+              <span class="ml-4">
+                Change your Tag:
+              </span>
+            </div>
+            <!--  Content that run through a loop of colours which at the same time set the colour of the chip
+              Make the code more maintainable as it will be easy to modify colour in future and get the index
+              Trigger a tagNotification when the chip is clicked (will take the colour as param)
+        -->
+            <div class="ml-3">
+              <v-chip
+                class="ma-1"
+                v-for="colour in colours"
+                :key=colour
+                :color="colour"
+                label
+                text-color="white"
+                @click="tagNotification(colour)"
+              >
+                <v-icon left>
+                  mdi-label
+                </v-icon>
+              </v-chip>
+            </div>
+          </v-card>
+        </v-expand-transition>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    model: 0,
+    colors: [
+      'primary',
+      'secondary',
+      'yellow darken-2',
+      'red',
+      'orange',
+    ],
+  }),
+};
+</script>
