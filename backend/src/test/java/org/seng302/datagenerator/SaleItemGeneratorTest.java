d2194461fa4e4125a4fb67cea391052965606941
Henry Barrett <hrb74@uclive.ac.nz>
2021-09-23 21:04:54 +1200

    S302T500-426 #time 20m added an extra test for SaleItemGenerator that ensures the inventoryItem quantity is updated as expected. Initially tried mocking the random number generated but then realised I could fetch the generated quantity from the saleItem ?test

--- backend/src/test/java/org/seng302/datagenerator/SaleItemGeneratorTest.java
+++ backend/src/test/java/org/seng302/datagenerator/SaleItemGeneratorTest.java
@@ -6,20 +6,25 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.seng302.leftovers.Main;
 import org.seng302.leftovers.persistence.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.sql.*;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes={Main.class})
@@ -73,7 +78,7 @@ class SaleItemGeneratorTest {
      * Checks that the required fields within the sale item table are not null using an SQL query.
      * @param saleItemId the id of the generated sale item
      */
-    public void checkRequiredFieldsNotNull(long saleItemId) throws SQLException {
+    private void checkRequiredFieldsNotNull(long saleItemId) throws SQLException {
       PreparedStatement stmt = conn.prepareStatement(
               "SELECT COUNT(*) FROM sale_item WHERE id = ? AND " +
                       "price IS NOT NULL AND quantity IS NOT NULL AND inventory_item_id IS NOT NULL"
@@ -89,12 +94,40 @@ class SaleItemGeneratorTest {
      * Queries that database to find out how many sale items are in the database
      * @return the number of sale items in the database
      */
-    public long getNumSaleItemsInDB() throws SQLException {
+    private long getNumSaleItemsInDB() throws SQLException {
       PreparedStatement stmt = conn.prepareStatement("SELECT COUNT(*) FROM sale_item");
       stmt.executeQuery();
       ResultSet results = stmt.getResultSet();
       results.next();
       return results.getLong(1);
+    }
+
+    /**
+     * Gets the quantity value for an inventory item with given id
+     * @param id ID of the inventory item to query
+     * @return Quantity of inventory item
+     */
+    private int getInventoryItemQuantity(Long id) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("SELECT quantity FROM inventory_item WHERE id = ?");
+        stmt.setObject(1, id);
+        stmt.executeQuery();
+        ResultSet resultSet = stmt.getResultSet();
+        resultSet.next();
+        return resultSet.getInt(1);
+    }
+
+    /**
+     * Gets the quantity value for an sale item with given id
+     * @param id ID of the sale item to query
+     * @return Quantity of sale item
+     */
+    private int getSaleItemQuantity(Long id) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("SELECT quantity FROM sale_item WHERE id = ?");
+        stmt.setObject(1, id);
+        stmt.executeQuery();
+        ResultSet resultSet = stmt.getResultSet();
+        resultSet.next();
+        return resultSet.getInt(1);
     }
 
     /**
@@ -159,6 +192,17 @@ class SaleItemGeneratorTest {
         assertEquals(1, saleItemIds.size());
         long saleItemId = saleItemIds.get(0);
         checkRequiredFieldsNotNull(saleItemId);
+    }
+
+    @Test
+    void generateSaleItems_inventoryQuantityUpdated() throws SQLException, IllegalAccessException, NoSuchFieldException {
+        List<Long> invItemIds = generateUserBusinessProductAndInvItems(1, 1, 1, 1);
+        var currentQuantity = getInventoryItemQuantity(invItemIds.get(0));
+
+        List<Long> saleIds = saleItemGenerator.generateSaleItems(invItemIds, 1);
+        var expectedQuantity = currentQuantity + getSaleItemQuantity(saleIds.get(0));
+
+        assertEquals(expectedQuantity, getInventoryItemQuantity(invItemIds.get(0)));
     }
 
 }
