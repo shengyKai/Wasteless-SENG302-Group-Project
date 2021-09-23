c1af218a073cf351bdfefef923ab118db00e9ae0
Nathan Smithies <nts36@uclive.ac.nz>
2021-09-23 12:59:06 +1200

    S302T500-428 #time 22m Added tested for purchased interested event ?test

---
+++ frontend/tests/unit/homepage/interest-purchased-event.spec.ts
@@ -0,0 +1,89 @@
+import Vue from "vue";
+import Vuetify from "vuetify";
+import {createLocalVue, mount, Wrapper} from "@vue/test-utils";
+import Vuex from "vuex";
+import * as api from '@/api/internal';
+import * as events from '@/api/events';
+import InterestPurchasedEvent from "@/components/home/newsfeed/InterestPurchasedEvent.vue";
+
+Vue.use(Vuetify);
+
+const business: api.Business = {
+  images:[],
+  primaryAdministratorId:2,
+  address:{
+    country:"Iceland",
+    streetName:"Racheal Road",
+    streetNumber:"711","city":"London",
+    district:"New Plymouth District",
+    postcode:"42105",
+    region:"Southland"},
+  created:"2021-09-14T01:29:16.496459Z",
+  name:"Hillary Cresenct Jewelers",
+  description:"Nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum nesciunt.",
+  id:1,
+  businessType:"Accommodation and Food Services"
+};
+const product: api.Product = {
+  countryOfSale: "Iceland",
+  images:[],
+  recommendedRetailPrice:842.92,
+  business,
+  created:"2021-09-14T01:29:16.497456Z",
+  name:"Complex Omelette",
+  description:"D quam  consequuntur! Commodi minima excepturi repudiandae velit hic maxime doloremque. Quaerat provident commodi consectetur veniam similique ad earum omnis ipsum saepe, volu.",
+  id:"FOXLVWMOW603801",
+  manufacturer:"Hoffman Incorporated"
+};
+
+const boughtSaleItem: api.BoughtSaleItem = {
+  id: 2,
+  buyer: null,
+  interestCount: 100,
+  quantity: 7,
+  product,
+  price: 50,
+  listingDate: "2021-09-14T01:29:16.497456Z",
+  saleDate: "2021-09-14T01:29:16.496459Z",
+};
+
+const event: events.InterestPurchasedEvent = {
+  id:1,
+  type:"InterestPurchasedEvent",
+  created:"2021-09-14T01:29:19.731330Z",
+  tag:"none","status":"normal",
+  lastModified:"2021-09-14T01:29:41.214058Z",
+  read: true,
+  boughtSaleItem,
+};
+describe("InterestEvent.vue", () => {
+  let wrapper: Wrapper<any>;
+  let vuetify: Vuetify;
+
+  beforeEach(async () => {
+    const localVue = createLocalVue();
+    vuetify = new Vuetify();
+
+    localVue.use(Vuex);
+    wrapper = mount(InterestPurchasedEvent, {
+      localVue,
+      vuetify,
+      propsData: {
+        event,
+      },
+      stubs: ['router-link'],
+    });
+  });
+
+  it("The event contains the product name", () => {
+    expect(wrapper.text()).toContain("Complex Omelette");
+  });
+
+  it("The event contains the business name", () => {
+    expect(wrapper.text()).toContain("Hillary Cresenct Jewelers");
+  });
+
+  it('Matches snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
