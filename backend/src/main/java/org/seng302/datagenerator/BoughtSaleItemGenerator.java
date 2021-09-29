99e2fac4d873b826ecd3dc4fe7413c6adbfa23ea
Ella Johnson <egj21@uclive.ac.nz>
2021-09-29 21:29:27 +1300

    S302T500-480 #time 20m Finished methods for generating bought sale items ?implement

--- backend/src/main/java/org/seng302/datagenerator/BoughtSaleItemGenerator.java
+++ backend/src/main/java/org/seng302/datagenerator/BoughtSaleItemGenerator.java
@@ -5,12 +5,16 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.sql.*;
+import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+
+import static org.seng302.datagenerator.Main.randomInstant;
 
 public class BoughtSaleItemGenerator {
     private Random random = new Random();
@@ -24,8 +28,8 @@ public class BoughtSaleItemGenerator {
      */
     @Value
     private static class SaleDates {
-        Instant saleDate;
         Instant listingDate;
+        Instant saleDate;
     }
 
     private Instant getUserCreationDate(long userId) throws SQLException {
@@ -57,12 +61,37 @@ public class BoughtSaleItemGenerator {
     private SaleDates generateSaleDates(long productId, long buyerId) throws SQLException {
         var productCreationDate = getProductCreationDate(productId);
         var buyerCreationDate = getUserCreationDate(buyerId);
-        throw new RuntimeException("Not yet implemented");
+        var minListingDate = Collections.max(List.of(productCreationDate, buyerCreationDate));
+        var listingDate = randomInstant(minListingDate, Instant.now());
+        var saleDate = randomInstant(listingDate, Instant.now());
+        return new SaleDates(listingDate, saleDate);
     }
 
 
     private long createInsertBoughtSaleItemSQL(long productId, long buyerId) throws SQLException {
-        throw new RuntimeException("Not yet implemented");
+        var likeCount = random.nextInt(500);
+        var price = (float) random.nextInt(100000) / 100;
+        var quantity = random.nextInt(100);
+        var saleDates = generateSaleDates(productId, buyerId);
+
+
+        try (PreparedStatement stmt = conn.prepareStatement(
+                "INSERT INTO bought_sale_item(like_count, listing_date, price, quantity, sale_date, buyer, product_id)"
+                        + "VALUES (?, ?, ?, ?, ?, ?, ?)",
+                Statement.RETURN_GENERATED_KEYS
+        )) {
+            stmt.setObject(1, likeCount);
+            stmt.setObject(2, saleDates.getListingDate());
+            stmt.setObject(3, price);
+            stmt.setObject(4, quantity);
+            stmt.setObject(5, saleDates.getSaleDate());
+            stmt.setObject(6, buyerId);
+            stmt.setObject(7, productId);
+            stmt.executeUpdate();
+            ResultSet keys = stmt.getGeneratedKeys();
+            keys.next();
+            return keys.getLong(1);
+        }
     }
 
     public List<Long> generateBoughtSaleItems(List<Long> productIds, List<Long> userIds, int boughtSaleItemCount) throws SQLException {
